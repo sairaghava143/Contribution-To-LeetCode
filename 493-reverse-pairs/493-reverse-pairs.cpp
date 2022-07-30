0aad57d1e6f0d3937e14bf4b0c8996fe2efb8ef3
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-07-30 14:46:01 +0530

    Time: 1461 ms (16.13%), Space: 237.4 MB (24.95%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,62 +1,46 @@
 class Solution {
 public:
-    int merge(vector<int> &nums,int start,int mid,int end){
-        int j=mid+1;
-        int count=0;
-        for(int i=start;i<=mid;i++){
-            while(j<=end && nums[i]>2LL*nums[j]){
+    int merge(vector<int>&nums,int start,int mid,int end){
+        int j=mid,count=0;
+        for(int i=start;i<mid;i++){
+            while(j<=end && nums[i]>2LL * nums[j]){
                 j++;
             }
-            count+=(j-(mid+1));
+            count+=(j-(mid));
         }
         vector<int>v;
-        int left=start,right=mid+1;
-        while(left<=mid && right<=end){
+        int left=start,right=mid;
+        while(left<mid && right<=end){
             if(nums[left]<=nums[right]){
-                v.push_back(nums[left]);
-                left++;
+                v.push_back(nums[left++]);
             }else{
-                v.push_back(nums[right]);
-                right++;
+                v.push_back(nums[right++]);
             }
         }
-        while(left<=mid){
-            v.push_back(nums[left]);
-            left++;
+        while(left<mid){
+            v.push_back(nums[left++]);
         }
-        while(right<=end){
-            v.push_back(nums[right]);
-            right++;
-            
+         while(right<=end){
+            v.push_back(nums[right++]);
         }
-        for(int i= start;i<=end;i++){
-            nums[i]=v[i-start];
+        
+        for(int i=start,k=0;i<=end;i++,k++){
+            nums[i]=v[k];
         }
         return count;
     }
-    int merge_sort(vector<int> &nums,int start,int end){
-        if(start>=end)return 0;
-        int mid=start+(end-start)/2;
-        int left=merge_sort(nums,start,mid);
-        int right=merge_sort(nums,mid+1,end);
-        int count=merge(nums,start,mid,end);
-        return count+left+right;
+    int merge_sort(vector<int>&nums,int start,int end){
+        int count=0;
+        if(start<end){
+            int mid=start+(end-start)/2;
+            int left=merge_sort(nums,start,mid);
+            int right=merge_sort(nums,mid+1,end);
+            int mycount=merge(nums,start,mid+1,end);
+            return left+right+mycount;
+        }
+        return count;
     }
     int reversePairs(vector<int>& nums) {
-        //time : O(N^2)
-        // int n=nums.size();
-        // int count=0;
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         long long x=nums[i],y=nums[j];
-        //         if(x>2*y){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // return count;
-        
-        //time : O(nlogn)
         return merge_sort(nums,0,nums.size()-1);
     }
 };
