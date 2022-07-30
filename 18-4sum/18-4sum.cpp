47c7b90f6c576f99d55fe0aefc3ae2d26c4af30c
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-07-31 04:25:09 +0530

    Time: 54 ms (88.69%), Space: 13.4 MB (31.97%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,9 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector< vector<int>>ans;
+//         vector< vector<int>>ans;
+//         if(nums.empty()){
+//             return ans;
+//         }
+//         sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 int left=j+1,right=n-1;
+//                 long long int sum=1ll*target-1ll*nums[i]-1ll*nums[j];
+//                 while(left<right){
+//                     long long int two_sum=1ll*nums[left]+1ll*nums[right];
+//                     if(two_sum<sum){
+//                         left++;
+//                     }else if(two_sum>sum){
+//                         right--;
+//                     }else{
+//                         ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+//                         int x=nums[left],y=nums[right];
+                    
+//                         while(left<right && nums[left]==x)left++;
+//                          while(left<right && nums[right]==y)right--;
+                        
+//                     }
+                  
+                    
+//                 }
+//                   while(j<n-1 && nums[j]==nums[j+1])j++;
+//             }
+//               while(i<n-1 && nums[i]==nums[i+1])i++;
+//         }
+ 
+//         return ans;
+        
+        ///using set
+        
+         set< vector<int>>ans;
+        vector<vector<int>>res;
         if(nums.empty()){
-            return ans;
+            return res;
         }
         sort(nums.begin(),nums.end());
         int n=nums.size();
@@ -18,22 +55,24 @@ public:
                     }else if(two_sum>sum){
                         right--;
                     }else{
-                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
-                        int x=nums[left],y=nums[right];
+                        ans.insert({nums[i],nums[j],nums[left],nums[right]});
+                         int x=nums[left],y=nums[right];
                     
                         while(left<right && nums[left]==x)left++;
                          while(left<right && nums[right]==y)right--;
-                        
                     }
                   
                     
                 }
-                  while(j<n-1 && nums[j]==nums[j+1])j++;
+                   while(j<n-1 && nums[j]==nums[j+1])j++;
             }
-              while(i<n-1 && nums[i]==nums[i+1])i++;
+               while(i<n-1 && nums[i]==nums[i+1])i++;
         }
- 
-        return ans;
+        for(auto i:ans){
+            res.push_back(i);
+        }
         
+ 
+        return res;
     }
 };
