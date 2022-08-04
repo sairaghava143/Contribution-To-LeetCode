30d6143ca8a21fee709c13f409389c8fc0d2c29d
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-04 11:47:25 +0530

    Time: 125 ms (66.18%), Space: 19.8 MB (98.25%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,32 +1,67 @@
 class Solution {
 public:
-
-      
-        
-    
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>v;
+//         set<vector<int>>ans;
+//         sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//                 int left=i+1,right=n-1;
+//             while(left<right){
+//                  int sum=nums[i]+nums[left]+nums[right];
+            
+//             if(sum==0){
+//                 ans.insert({nums[i],nums[left],nums[right]});
+//                 left++;
+//                 right--;
+//             }else if(sum<0){
+//                                 left++;
+
+//             }else{
+//                                 right--;
+
+//             }
+                
+//             }
+           
+            
+//         }
+//         vector<vector<int>>v;
+//    for(auto it:ans){
+//        v.push_back(it);
+//    }
+        
+//         return v;
+//     }
+        
+        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-2;i++){
+        int n=nums.size();
+        for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1])continue;
-            int left=i+1;int right=nums.size()-1;
-            int targetsum=nums[i];
-           while(left<right){
-              int sum=targetsum+nums[left]+nums[right];
-            if(sum>0){
-                right--;
-            }else if(sum<0){
-                left++;
-            }else{
-                 v.push_back({targetsum,nums[left],nums[right]});
+                int left=i+1,right=n-1;
+            while(left<right){
+                 int sum=nums[i]+nums[left]+nums[right];
+            
+            if(sum==0){
+                ans.push_back({nums[i],nums[left],nums[right]});
                 while(left<right && nums[left]==nums[left+1])left++;
                 while(left<right && nums[right]==nums[right-1])right--;
                 left++;
                 right--;
+            }else if(sum<0){
+                                left++;
+
+            }else{
+                                right--;
+
+            }
+                
             }
            
+            
         }
-        }
-        return v;
+
+        
+        return ans;
     }
 };
