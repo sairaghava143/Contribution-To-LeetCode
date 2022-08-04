ed35ccb6a3fdd2dbb9493c9703a7a19822c99968
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-04 18:00:02 +0530

    Time: 15 ms (92.02%), Space: 19.5 MB (99.94%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,93 +1,29 @@
 class Solution {
 public:
-    // int lefth(vector<int>v,int j){
-    //     int ans=0;
-    //     for(int i=j;i>=0;i--){
-    //         ans=max(ans,v[i]);
-    //     }
-    //     return ans;
-    // }
-    // int righth(vector<int>v,int j){
-    //     int ans=0;
-    //     for(int i=j;i<v.size();i++){
-    //         ans=max(ans,v[i]);
-    //     }
-    //     return ans;
-    // }
-    //  int lefth(vector<int>v,int j){
-    //     int ans=0;
-    //     for(int i=0;i<=j;i++){
-    //         ans=max(ans,v[i]);
-    //     }
-    //     return ans;
-    // }
-    // int righth(vector<int>v,int j){
-    //     int ans=0;
-    //     for(int i=j;i<v.size();i++){
-    //         ans=max(ans,v[i]);
-    //     }
-    //     return ans;
-    // }
     int trap(vector<int>& height) {
-        //brute force
-        // int n=height.size();
-        // int totalwater=0;
-        // for(int i=0;i<n;i++){
-        //     int leftmax=lefth(height,i);
-        //     int rightmax=righth(height,i);
-        //     int currentwater=min(leftmax,rightmax)-height[i];
-        //     totalwater+=currentwater;
-        // }
-        // return totalwater;
-        
-        ///better brute force approach
-        // int n=height.size();
-        // vector<int>leftmax(n),rightmax(n);
-        // int ans=0;
-        // for(int i=0;i<n;i++){
-        //     ans=max(ans,height[i]);
-        //     leftmax[i]=ans;
-        // }
-        // ans=0;
-        // for(int i=n-1;i>=0;i--){
-        //     ans=max(ans,height[i]);
-        //     rightmax[i]=ans;
-        // }
-        // int totalans=0;
-        // for(int i=0;i<n;i++){
-        //   totalans+=min(leftmax[i],rightmax[i])-height[i];
-        // }
-        // return totalans;
-        
-        ///optimized two pointer approach
         int n=height.size();
-        int left=0,right=n-1;
-        int leftmax=0,rightmax=0,totalans=0;
+        int left=0,right=n-1,leftmax=0,rightmax=0,ans=0;
         while(left<=right){
             if(height[left]<=height[right]){
                 if(height[left]>=leftmax){
-                leftmax=height[left];
+                    leftmax=height[left];
+                    
+                }else{
+                    ans+=leftmax-height[left];
+                    
+                }
+                left++;
             }else{
-                totalans+=leftmax-height[left];
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
+                    
+                }else{
+                    ans+=rightmax-height[right];
+                    
+                }
+                right--;
             }
-                                left++;
-
-            }else{
-                 if(height[right]>=rightmax){
-                rightmax=height[right];
-            }else{
-                totalans+=rightmax-height[right];
-            }
-                                right--;
-
-            }
-            
-           
-            
         }
-        
-        return totalans;
-        
-        
+        return ans;
     }
 };
