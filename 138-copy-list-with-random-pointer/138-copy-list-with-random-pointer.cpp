5f9981d7b8ecd756cf6593f6afec02798efe1828
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-04 11:15:57 +0530

    Time: 20 ms (20.16%), Space: 11.1 MB (88.20%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,19 +17,50 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*>m;
-        Node *ptr=head;
-        while(ptr){
-            m[ptr]=new Node(ptr->val);
-            ptr=ptr->next;
-        }
-        ptr=head;
-        while(ptr){
-            m[ptr]->next=m[ptr->next];
-            m[ptr]->random=m[ptr->random];
-            ptr=ptr->next;
-        }
-        return m[head];
+        //time O(n)  space O(n)
+        // unordered_map<Node*,Node*>m;
+        // Node *ptr=head;
+        // while(ptr){
+        //     m[ptr]=new Node(ptr->val);
+        //     ptr=ptr->next;
+        // }
+        // ptr=head;
+        // while(ptr){
+        //     m[ptr]->next=m[ptr->next];
+        //     m[ptr]->random=m[ptr->random];
+        //     ptr=ptr->next;
+        // }
+        // return m[head];
         
+        
+          //time O(n)  space O(1)
+        
+        Node *node=head;
+        while(node){
+            Node *temp=node->next;
+            node->next=new Node(node->val);
+            node->next->next=temp;
+            node=temp;
+        }
+        node =head;
+        while(node){
+            if(node->random){
+                node->next->random=node->random->next;
+            }
+            node=node->next->next;
+        }
+        
+        Node *ans=new Node(0);
+        Node *temp=ans;
+      
+        node=head;
+        while(node){
+            temp->next=node->next;
+            temp=temp->next;
+            
+            node->next=node->next->next;
+            node=node->next;
+        }
+        return ans->next;
     }
 };
