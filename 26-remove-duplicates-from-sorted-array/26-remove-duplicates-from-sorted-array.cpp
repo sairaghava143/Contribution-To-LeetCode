f44b2c0fd0caeb64f022db8ae43e282772dcde99
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-04 19:15:39 +0530

    Time: 19 ms (56.07%), Space: 18.3 MB (93.15%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n=nums.size();
+        //brute force approach
+//         int n=nums.size();
         
-     auto it=std::unique(nums.begin(),nums.end());
+//      auto it=std::unique(nums.begin(),nums.end());
         
-nums.resize(std::distance(nums.begin(),it));
-    return nums.size();
+// nums.resize(std::distance(nums.begin(),it));
+//     return nums.size();
+        
+        //optimized approach sliding pattern
+        if(nums.empty())return 0;
+        
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+    return i+1;
     }
 };
