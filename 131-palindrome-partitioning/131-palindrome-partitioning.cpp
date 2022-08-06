d69b3bf621f4003408803d1f6027f9b6f269f5a4
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-06 14:45:40 +0530

    Time: 567 ms (5.79%), Space: 144.7 MB (17.84%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -15,18 +15,18 @@ public:
             ans.push_back(temp);
             return;
         }
+        
         for(int i=index;i<s.size();i++){
             if(isPalindrom(s,index,i)){
                 temp.push_back(s.substr(index,i-index+1));
                 subset(ans,temp,i+1,s);
                 temp.pop_back();
             }
-            // subset(ans,temp,i+1,s);
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
-        vector<string> temp;
+        vector<string>temp;
         subset(ans,temp,0,s);
         return ans;
     }
