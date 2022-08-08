8b3f3f093d6042602219f7b303dba5534a5b525a
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-08 20:10:23 +0530

    Time: 47 ms (48.21%), Space: 6.4 MB (49.84%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -23,19 +23,19 @@ public:
         }
         return true;
     }
-    bool isValid(int row,int col,char c,vector<vector<char>>board){
+    bool isValid(int row,int col,char c,vector<vector<char>>&board){
         for(int i=0;i<9;i++){
             if(board[row][i]==c)return false;
             if(board[i][col]==c)return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)return false;
+            // if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)return false;
         }
-//         int x=row/3*3,y=col/3*3;
-//         for(int i=0;i<3;i++){
-//             for(int j=0;j<3;j++){
-//                 if(board[x+i][y+j]==c)return false;
+        int x=row/3*3,y=col/3*3;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(board[x+i][y+j]==c)return false;
                 
-//             }
-//         }
+            }
+        }
         
         return true;
     }
