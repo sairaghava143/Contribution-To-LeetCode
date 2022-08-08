118f910e60da1a2aa4d6de7ce578d72ce856462f
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-08 19:06:33 +0530

    Time: 30 ms (23.61%), Space: 15.9 MB (18.39%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -35,18 +35,48 @@ public:
             }
         }
     }
+    
+    void solve2(vector<vector<string>>&ans,vector<string>s,int n,int col,vector<int>rowleft,vector<int>lowerbound,vector<int>upperbound){
+        if(col==n){
+            ans.push_back(s);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(rowleft[row]==0 && lowerbound[row+col]==0 && upperbound[n-1+col-row]==0){
+                s[row][col]='Q';
+                rowleft[row]=1;
+                lowerbound[row+col]=1;
+                upperbound[n-1+col-row]=1;
+                solve2(ans,s,n,col+1,rowleft,lowerbound,upperbound);
+                s[row][col]='.';
+                rowleft[row]=0;
+                lowerbound[row+col]=0;
+                upperbound[n-1+col-row]=0;
+            }
+        }
+    }
     vector<vector<string>> solveNQueens(int n) {
         ///brute force
+//         vector<vector<string>>ans;
+//         vector<string>s(n);
+//         string st(n,'.');
+//         for(int i=0;i< n;i++){
+//             s[i]=st;
+//         }
+        
+//         solve(ans,s,n,0);
+//         return ans;
+        
+        //optimized approach
         vector<vector<string>>ans;
+        vector<int> rowleft(n,0),lowerbound(2*n-1,0),upperbound(2*n-1,0);
         vector<string>s(n);
         string st(n,'.');
         for(int i=0;i< n;i++){
             s[i]=st;
         }
         
-        solve(ans,s,n,0);
+        solve2(ans,s,n,0,rowleft,lowerbound,upperbound);
         return ans;
-        
-        //optimized approach
     }
 };
