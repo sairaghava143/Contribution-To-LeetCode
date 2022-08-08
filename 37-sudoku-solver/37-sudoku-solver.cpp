6e54a8649abfd9d54b2996b3bb718cc1d485f07e
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-08 20:07:35 +0530

    Time: 1522 ms (5.00%), Space: 318.7 MB (5.00%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,26 +1,20 @@
-class Solution
-{
+class Solution {
 public:
-    void solveSudoku(vector<vector<char>> &board)
-    {
+    void solveSudoku(vector<vector<char>>& board) {
         solve(board);
     }
-    bool solve(vector<vector<char>> &board)
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-            {
-                if (board[i][j] == '.')
-                {
-                    for (char c = '1'; c <= '9'; c++)
-                    {
-                        if (isValid(board, i, j, c))
-                        {
-                            board[i][j] = c;
-                            if (solve(board))
+    bool solve(vector<vector<char>>&board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char c='1';c<='9';c++){
+                        if(isValid(i,j,c,board)){
+                            board[i][j]=c;
+                            if(solve(board)){
                                 return true;
-                            board[i][j] = '.';
+                            }else{
+                                board[i][j]='.';
+                            }
                         }
                     }
                     return false;
@@ -29,26 +23,20 @@ public:
         }
         return true;
     }
-    bool isValid(vector<vector<char>> &board, int i, int j, char c)
-    {
-        for (int k = 0; k < 9; k++)
-        {
-            if (board[i][k] == c)
-                return false;
-            if (board[k][j] == c)
-                return false;
+    bool isValid(int row,int col,char c,vector<vector<char>>board){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c)return false;
+            if(board[i][col]==c)return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)return false;
         }
-        int x = i / 3 * 3;
-        int y = j / 3 * 3;
-        for (int k = 0; k < 3; k++)
-        {
-            for (int l = 0; l < 3; l++)
-            {
-                if (board[x + k][y + l] == c)
-                    return false;
-            }
-        }
+//         int x=row/3*3,y=col/3*3;
+//         for(int i=0;i<3;i++){
+//             for(int j=0;j<3;j++){
+//                 if(board[x+i][y+j]==c)return false;
+                
+//             }
+//         }
+        
         return true;
     }
 };
-
