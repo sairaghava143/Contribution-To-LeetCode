a18bf3e00c7c45615178427b998468d7ab1e814d
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-10 17:31:36 +0530

    Time: 85 ms (17.81%), Space: 89.2 MB (86.59%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -9,11 +9,32 @@ public:
         while(s<=e){
             int cut1=(s+e)>>1;
             int cut2=(n1+n2+1)/2-cut1;
+            int l1,l2,r1,r2;
+            if(cut1==0){
+                l1=INT_MIN;
+            }else{
+                l1=nums1[cut1-1];
+            }
+             if(cut2==0){
+                l2=INT_MIN;
+            }else{
+                l2=nums2[cut2-1];
+            }
+             if(cut1==n1){
+                r1=INT_MAX;
+            }else{
+                r1=nums1[cut1];
+            }
+             if(cut2==n2){
+                r2=INT_MAX;
+            }else{
+                r2=nums2[cut2];
+            }
             
-        int l1=cut1==0?INT_MIN:nums1[cut1-1];
-            int l2=cut2==0?INT_MIN:nums2[cut2-1];
-            int r1=cut1==n1?INT_MAX:nums1[cut1];
-            int r2=cut2==n2?INT_MAX:nums2[cut2];
+        // int l1=cut1==0?INT_MIN:nums1[cut1-1];
+        //     int l2=cut2==0?INT_MIN:nums2[cut2-1];
+        //     int r1=cut1==n1?INT_MAX:nums1[cut1];
+        //     int r2=cut2==n2?INT_MAX:nums2[cut2];
             if(l1<=r2 && l2<=r1){
                 if((n1+n2)&1){
                     return max(l1,l2);
