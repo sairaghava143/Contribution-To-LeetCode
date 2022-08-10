b03224ed34490e7a5113dc97bafea1f78ce8f7dd
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-10 10:57:18 +0530

    Time: 4 ms (72.82%), Space: 11.1 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -4,9 +4,9 @@ public:
         int s=0,e=nums.size()-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-             if(nums[mid]==target){
-                 return mid;
-             }
+            if(nums[mid]==target){
+                return mid;
+            }
             if(nums[s]<=nums[mid]){
                 if(nums[s]<=target && nums[mid]>=target){
                     e=mid-1;
@@ -18,11 +18,9 @@ public:
                 if(nums[mid]<=target && nums[e]>=target){
                     s=mid+1;
                 }else{
-                    e=mid-1;
+                   e=mid-1;
                 }
             }
-            
-           
             
         }
         return -1;
