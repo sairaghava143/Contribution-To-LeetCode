d1ed9dd6d4f271eb4fe40e944621dcc732086050
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-12 14:42:29 +0530

    Time: 198 ms (9.58%), Space: 45.4 MB (15.37%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -36,14 +36,51 @@ public:
         //parial sort
         // partial_sort(nums.begin(),nums.begin()+k,nums.end(),greater<int>());
         // return nums[k-1];
-        multiset<int> mset;
-        for (int num : nums) {
-            mset.insert(num);
-            if (mset.size() > k) {
-                mset.erase(mset.begin());
-            }
+        // multiset<int> mset;
+        // for (int num : nums) {
+        //     mset.insert(num);
+        //     if (mset.size() > k) {
+        //         mset.erase(mset.begin());
+        //     }
+        // }
+        // return *mset.begin();
+        buildMaxHeap(nums);
+        for (int i = 0; i < k - 1; i++) {
+            swap(nums[0], nums[--heapSize]);
+            maxHeapify(nums, 0);
         }
-        return *mset.begin();
+        return nums[0];
+    }
+private:
+    int heapSize;
+    
+    int left(int i) {
+        return (i << 1) + 1;
+    }
+    
+    int right(int i) {
+        return (i << 1) + 2;
+    }
+    
+    void maxHeapify(vector<int>& nums, int i) {
+        int largest = i, l = left(i), r = right(i);
+        if (l < heapSize && nums[l] > nums[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && nums[r] > nums[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            maxHeapify(nums, largest);
+        }
+    }
+    
+    void buildMaxHeap(vector<int>& nums) {
+        heapSize = nums.size();
+        for (int i = (heapSize >> 1) - 1; i >= 0; i--) {
+            maxHeapify(nums, i);
+        }
 
     }
 };
