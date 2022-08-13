700ca7ed783e0e5dcce4d265c976ca3df75e060d
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-13 19:14:09 +0530

    Time: 6 ms (83.68%), Space: 8.8 MB (70.44%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
-//         stack<int>s;
-//         unordered_map<int,int>m;
-//         for(int it:nums2){
-//             if(s.size() && s.top()<it){
-//                 m[s.top()]=it;
-//                 s.pop();
-//             }
-// s.push(it);
-
-            
-//         }
-//         vector<int>ans;
-//         for(auto it:nums1){
-//             ans.push_back(m.count(it)?m[it]:-1);
-//         }
-//         return ans;
-    stack<int> s;
-        unordered_map<int, int> m;
-        for (int n : nums) {
-            while (s.size() && s.top() < n) {
-                m[s.top()] = n;
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>s;
+        unordered_map<int,int>m;
+        for(int it:nums2){
+            while(s.size() && s.top()<it){
+                m[s.top()]=it;
                 s.pop();
             }
-            s.push(n);
+s.push(it);
+
+            
         }
-        vector<int> ans;
-        for (int n : findNums) ans.push_back(m.count(n) ? m[n] : -1);
+        vector<int>ans;
+        for(auto it:nums1){
+            ans.push_back(m.count(it)?m[it]:-1);
+        }
         return ans;
     }
 };
