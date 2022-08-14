cc605d90364a056a37a4c9349db18d3493b62c51
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-14 15:02:51 +0530

    Time: 717 ms (42.13%), Space: 165.1 MB (81.09%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,38 +1,36 @@
-class LRUCache
-{
+class LRUCache {
 public:
-    list<pair<int, int>> l;
-    unordered_map<int, list<pair<int, int>>::iterator> m;
-    int size;
-    LRUCache(int capacity)
-    {
-        size = capacity;
+    list<pair<int,int>>l;
+    unordered_map<int,list<pair<int,int>>::iterator>m;
+    int cp;
+    LRUCache(int capacity) {
+        cp=capacity;
     }
-
-    int get(int key)
-    {
-        if (m.find(key) == m.end())
-            return -1;
-        l.splice(l.begin(), l, m[key]);
+    
+    int get(int key) {
+        if(!m.count(key))return -1;
+        l.splice(l.begin(),l,m[key]);
         return m[key]->second;
     }
-
-    void put(int key, int value)
-    {
-        if (m.find(key) != m.end())
-        {
-            l.splice(l.begin(), l, m[key]);
-            m[key]->second = value;
-            return;
-        }
-        if (l.size() == size)
-        {
-            auto d_key = l.back().first;
+    
+    void put(int key, int value) {
+       if(m.count(key)){
+           l.splice(l.begin(),l,m[key]);
+           m[key]->second=value;
+           
+           return;
+           
+           
+       }
+        if(cp==l.size()){
+            auto b=l.back().first;
             l.pop_back();
-            m.erase(d_key);
+            m.erase(b);
         }
-        l.push_front({key, value});
-        m[key] = l.begin();
+        l.push_front({key,value});
+        m[key]=l.begin();
+            
+        
     }
 };
 
