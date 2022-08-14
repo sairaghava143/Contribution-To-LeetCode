ef04b34beb3d9d9eaf7f0a8ec124c2c036c5b2aa
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-14 14:26:20 +0530

    Time: 453 ms (97.38%), Space: 164.9 MB (86.03%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,70 +1,39 @@
-class LRUCache {
+class LRUCache
+{
 public:
-        class node {
-      public:
-        int key;
-      int val;
-      node * next;
-      node * prev;
-      node(int _key, int _val) {
-        key = _key;
-        val = _val;
-      }
-    };
-     node * head = new node(-1, -1);
-  node * tail = new node(-1, -1);
-
-  int cap;
-map < int, node * > m;
-    LRUCache(int capacity) {
-         cap = capacity;
-    head -> next = tail;
-    tail -> prev = head;
-    }
-    
-  void addnode(node * newnode) {
-    node * temp = head -> next;
-    newnode -> next = temp;
-    newnode -> prev = head;
-    head -> next = newnode;
-    temp -> prev = newnode;
-  }
-
-  void deletenode(node * delnode) {
-    node * delprev = delnode -> prev;
-    node * delnext = delnode -> next;
-    delprev -> next = delnext;
-    delnext -> prev = delprev;
-  }
-    int get(int key_) {
-         if (m.find(key_) != m.end()) {
-      node * resnode = m[key_];
-      int res = resnode -> val;
-      m.erase(key_);
-      deletenode(resnode);
-      addnode(resnode);
-      m[key_] = head -> next;
-      return res;
+    list<pair<int, int>> l;
+    unordered_map<int, list<pair<int, int>>::iterator> m;
+    int size;
+    LRUCache(int capacity)
+    {
+        size = capacity;
     }
 
-    return -1;
-    }
-    
-    void put(int key_, int value) {
-         if (m.find(key_) != m.end()) {
-      node * existingnode = m[key_];
-      m.erase(key_);
-      deletenode(existingnode);
-    }
-    if (m.size() == cap) {
-      m.erase(tail -> prev -> key);
-      deletenode(tail -> prev);
+    int get(int key)
+    {
+        if (m.find(key) == m.end())
+            return -1;
+        l.splice(l.begin(), l, m[key]);
+        return m[key]->second;
     }
 
-    addnode(new node(key_, value));
-    m[key_] = head -> next;
-  }
-    
+    void put(int key, int value)
+    {
+        if (m.find(key) != m.end())
+        {
+            l.splice(l.begin(), l, m[key]);
+            m[key]->second = value;
+            return;
+        }
+        if (l.size() == size)
+        {
+            auto d_key = l.back().first;
+            l.pop_back();
+            m.erase(d_key);
+        }
+        l.push_front({key, value});
+        m[key] = l.begin();
+    }
 };
 
 /**
