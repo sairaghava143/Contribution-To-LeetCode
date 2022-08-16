451c543c26a1499b328c72e7e307a1626f0c0e30
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-16 05:57:26 +0530

    Time: 15 ms (93.47%), Space: 6.6 MB (100.00%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    void helper(int &st,int &e,string &s,int &maxlen,int l,int r,int &n){
-        while(l>=0 && r<n ){
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int st=0,e=0,maxlen=1;
+        //odd
+        for(int i=0;i<n;i++){
+            int l=i;
+            int r=i;
+            while(l>=0 && r<n ){
                 if(s[l]==s[r]){
-                   l--;
-                r++; 
+                    l--;
+                    r++;
                 }else{
                     break;
                 }
-                
             }
-                int length=r-l-1;
+            int length=r-l-1;
             if(length>maxlen){
                 maxlen=length;
                 st=l+1;
                 e=r-1;
             }
-    }
-    string longestPalindrome(string s) {
-        int st=0,e=0,maxlen=1;
-        int n=s.size();
-        //odd
-    
-            for(int i=0;i<n;i++){
-            int l=i;
-            int r=i;
-                helper(st,e,s,maxlen,l,r,n);
-            
         }
-      
          //even
-    
-            for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             int l=i;
             int r=i+1;
-                           helper(st,e,s,maxlen,l,r,n);
-
+            while(l>=0 && r<n ){
+                if(s[l]==s[r]){
+                    l--;
+                    r++;
+                }else{
+                    break;
+                }
+            }
+            int length=r-l-1;
+            if(length>maxlen){
+                maxlen=length;
+                st=l+1;
+                e=r-1;
+            }
         }
-        
         return s.substr(st,maxlen);
-        
     }
 };
