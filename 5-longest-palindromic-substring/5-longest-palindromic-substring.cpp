7b7f486542a13c98e567ab063b8dfdc6e8ba32eb
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-16 05:53:52 +0530

    Time: 18 ms (92.09%), Space: 6.9 MB (97.46%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    void helper(int &st,int &e,string &s,int &maxlen,int l,int r,int &n){
+        while(l>=0 && r<n ){
+                if(s[l]==s[r]){
+                   l--;
+                r++; 
+                }else{
+                    break;
+                }
+                
+            }
+                int length=r-l-1;
+            if(length>maxlen){
+                maxlen=length;
+                st=l+1;
+                e=r-1;
+            }
+    }
     string longestPalindrome(string s) {
         int st=0,e=0,maxlen=1;
         int n=s.size();
@@ -8,21 +25,8 @@ public:
             for(int i=0;i<n;i++){
             int l=i;
             int r=i;
-            while(l>=0 && r<n ){
-                if(s[l]==s[r]){
-                   l--;
-                r++; 
-                }else{
-                    break;
-                }
-                
-            }
-                int length=r-l-1;
-            if(length>maxlen){
-                maxlen=length;
-                st=l+1;
-                e=r-1;
-            }
+                helper(st,e,s,maxlen,l,r,n);
+            
         }
       
          //even
@@ -30,21 +34,8 @@ public:
             for(int i=0;i<n;i++){
             int l=i;
             int r=i+1;
-            while(l>=0 && r<n ){
-                if(s[l]==s[r]){
-                   l--;
-                r++; 
-                }else{
-                    break;
-                }
-                
-            }
-                int length=r-l-1;
-            if(length>maxlen){
-                maxlen=length;
-                st=l+1;
-                e=r-1;
-            }
+                           helper(st,e,s,maxlen,l,r,n);
+
         }
         
         return s.substr(st,maxlen);
