b7f36c1d13f447ab3cbe5e93fc7c04e89acb1a1c
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-17 12:45:29 +0530

    Time: 9 ms (74.99%), Space: 7 MB (84.75%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        int n=a.size();
-        int m=b.size();
-    
         string temp=a;
         int count=1;
         while(a.size()<b.size()){
             a+=temp;
             count++;
         }
-     
-        if(a.find(b)!=string::npos){
-            return count;
-        }
+        if(a.find(b)!=string::npos)return count;
         a+=temp;
-        if(a.find(b)!=string::npos){
-            return count+1;
-        }
+        if(a.find(b)!=string::npos)return count+1;
         return -1;
-        
     }
 };
