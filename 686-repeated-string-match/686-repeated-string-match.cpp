b72570d825cf46a7e7bbc1054309fa9faad78756
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-17 11:46:40 +0530

    Time: 1733 ms (5.46%), Space: 7.5 MB (35.23%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -6,14 +6,13 @@ public:
         for(int i=0;i<=n-m;i++){
             int j;
             for(j=0;j<m;j++){
-                if(a[i+j]!=b[j]){
-                    break;
-                }
+                if(a[i+j]!=b[j])break;
             }
-                            if(j==m)return 1;
-
+            if(j==m)return 1;
         }
         return 0;
+        
+        
     }
     int repeatedStringMatch(string a, string b) {
         string temp=a;
@@ -21,13 +20,13 @@ public:
         while(a.size()<b.size()){
             a+=temp;
             ans++;
-            }
+        }
         if(check(a,b)){
-                return ans;
-            }
-            if(check(a+temp,b)){
-                return ans+1;
-            }
+            return ans;
+        }
+        if(check(a+temp,b)){
+            return ans+1;
+        }
         return -1;
     }
 };
