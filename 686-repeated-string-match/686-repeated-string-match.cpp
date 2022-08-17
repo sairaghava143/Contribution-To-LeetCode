cbc73173333b54789109ee708909dc801f379ee8
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-17 11:34:40 +0530

    Time: 1545 ms (7.16%), Space: 7.4 MB (44.58%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool check(string a,string b){
+        int n=a.size();
+        int m=b.size();
+        for(int i=0;i<=n-m;i++){
+            int j;
+            for(j=0;j<m;j++){
+                if(a[i+j]!=b[j]){
+                    break;
+                }
+            }
+                            if(j==m)return 1;
+
+        }
+        return 0;
+    }
+    int repeatedStringMatch(string a, string b) {
+        string temp=a;
+        int ans=1;
+        while(a.size()<b.size()){
+            a+=temp;
+            ans++;
+            }
+        if(check(a,b)){
+                return ans;
+            }
+            if(check(a+temp,b)){
+                return ans+1;
+            }
+        return -1;
+    }
+};
