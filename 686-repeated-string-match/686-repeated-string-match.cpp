990041df7e325a261c33e17316489a1904b2c6a9
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-17 12:35:58 +0530

    Time: 13 ms (59.04%), Space: 7 MB (84.75%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    //learnt new thing find substring
-    bool check(string a,string b){
+    int repeatedStringMatch(string a, string b) {
         int n=a.size();
         int m=b.size();
-        for(int i=0;i<=n-m;i++){
-            int j;
-            for(j=0;j<m;j++){
-                if(a[i+j]!=b[j])break;
-            }
-            if(j==m)return 1;
-        }
-        return 0;
-        
-        
-    }
-    int repeatedStringMatch(string a, string b) {
+    
         string temp=a;
-        int ans=1;
+        int count=1;
         while(a.size()<b.size()){
             a+=temp;
-            ans++;
+            count++;
         }
-        if(check(a,b)){
-            return ans;
+     
+        if(a.find(b)!=string::npos){
+            return count;
         }
-        if(check(a+temp,b)){
-            return ans+1;
+        a+=temp;
+        if(a.find(b)!=string::npos){
+            return count+1;
         }
         return -1;
+        
     }
 };
