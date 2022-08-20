be159118a60935141be5efe6edd99375464c5515
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-20 14:10:31 +0530

    Time: 21 ms (21.70%), Space: 17.3 MB (53.04%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -15,7 +15,7 @@ public:
         queue<pair<TreeNode*,int>>q;
         q.push({root,0});
         long long ans=INT_MIN;
-        while(!q.empty()){
+        while(q.size()){
             int size=q.size();
             long long start=q.front().second;
             long long end=q.back().second;
@@ -24,15 +24,16 @@ public:
                 auto node=q.front();
                 q.pop();
                 long long index=node.second;
-                if(node.first->left){
-                    q.push({node.first->left,2*index+1});
+                TreeNode *m=node.first;
+                if(m->left){
+                    q.push({m->left,2*index+1});
                 }
-                if(node.first->right){
-                    q.push({node.first->right,2*index+2});
+                 if(m->right){
+                    q.push({m->right,2*index+2});
                 }
-            }
                 
+            }
         }
-        return int(ans);
+        return ans;
     }
 };
