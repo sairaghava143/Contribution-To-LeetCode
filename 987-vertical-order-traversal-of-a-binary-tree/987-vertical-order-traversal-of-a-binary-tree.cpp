0e26c53df1dedc09acba245eb7535abc64514571
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-20 11:12:30 +0530

    Time: 8 ms (58.23%), Space: 15.5 MB (6.77%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,37 +11,57 @@
  */
 class Solution {
 public:
+    map<int,map<int,multiset<int>>>m;
+    void solve(TreeNode* root,int col,int row){
+        if(root==NULL)return ;
+        m[col][row].insert(root->val);
+        solve(root->left,col-1,row+1);
+        solve(root->right,col+1,row+1);
+    }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         //bfs
-        map<int,vector<int>>m;
-        queue<pair<TreeNode*,int>>q;
-        q.push({root,0});
-        while(!q.empty()){
-            int size=q.size();
-            map<int,multiset<int>>mapset;
-            for(int i=0;i<size;i++){
-                auto node=q.front();
-                q.pop();
-                int col=node.second;
-                mapset[col].insert(node.first->val);
-                if(node.first->left){
-                    q.push({node.first->left,col-1});
-                }
-                 if(node.first->right){
-                    q.push({node.first->right,col+1});
-                }
-            }
-            for(auto it:mapset){
-                for(auto it2:it.second){
-                    m[it.first].push_back(it2);
-                }
-            }
+//         map<int,vector<int>>m;
+//         queue<pair<TreeNode*,int>>q;
+//         q.push({root,0});
+//         while(!q.empty()){
+//             int size=q.size();
+//             map<int,multiset<int>>mapset;
+//             for(int i=0;i<size;i++){
+//                 auto node=q.front();
+//                 q.pop();
+//                 int col=node.second;
+//                 mapset[col].insert(node.first->val);
+//                 if(node.first->left){
+//                     q.push({node.first->left,col-1});
+//                 }
+//                  if(node.first->right){
+//                     q.push({node.first->right,col+1});
+//                 }
+//             }
+//             for(auto it:mapset){
+//                 for(auto it2:it.second){
+//                     m[it.first].push_back(it2);
+//                 }
+//             }
             
-        }
+//         }
+//         vector<vector<int>>v;
+//          for(auto it:m){
+//                v.push_back(it.second);
+//             }
+//         return v;
+        //dfs
+        solve(root,0,0);
         vector<vector<int>>v;
-         for(auto it:m){
-               v.push_back(it.second);
+        for(auto it:m){
+            vector<int>ans;
+            for(auto it2:it.second){
+                for(auto it3:it2.second){
+                    ans.push_back(it3);
+                }
             }
+            v.push_back(ans);
+        }
         return v;
     }
 };
