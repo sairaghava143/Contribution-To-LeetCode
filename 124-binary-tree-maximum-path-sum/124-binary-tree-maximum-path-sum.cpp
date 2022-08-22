23233fd5cafb542854567e4b5e3c9727f4f31819
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-22 15:51:18 +0530

    Time: 58 ms (13.47%), Space: 28.3 MB (22.84%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -11,45 +11,21 @@
  */
 class Solution {
 public:
-    int maxsum(TreeNode* root,int &ans){
+    int maxsum(TreeNode *root,int &ans){
         if(root==NULL)return 0;
         int left=maxsum(root->left,ans);
         int right=maxsum(root->right,ans);
-      int max_straight=max(max(left,right)+root->val,root->val);
+        int max_straight=max(max(left,right)+root->val,root->val);
         int max_case=max(max_straight,left+right+root->val);
-        
-        ans=max(ans,max_case);
+        ans=max(max_case,ans);
         return max_straight;
+
     }
-    
     int maxPathSum(TreeNode* root) {
+        if(root==NULL)return 0;
         int ans=INT_MIN;
-     maxsum(root,ans);
+        maxsum(root,ans);
         return ans;
+            
     }
 };
-// class Solution {
-//     int maxPathSum_util(TreeNode *root,int &result)
-//     {
-//         if(!root)
-//             return 0;
-        
-//         int left = maxPathSum_util(root->left,result);
-//         int right = maxPathSum_util(root->right,result);
-        
-//         int max_straight = max(max(left,right)+root->val, root->val);
-//         int max_caseVal = max(max_straight,left+right+root->val);
-//         result = max(max_caseVal,result);
-        
-//         return max_straight;
-//     }
-// public:
-//     int maxPathSum(TreeNode* root) {
-//         ios_base::sync_with_stdio(false);
-//         cin.tie(NULL);
-        
-//         int result = INT_MIN;
-//         maxPathSum_util(root,result);
-//         return result; 
-//     }
-// };
