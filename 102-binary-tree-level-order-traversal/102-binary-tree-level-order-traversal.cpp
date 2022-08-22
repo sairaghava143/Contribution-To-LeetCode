9714d9cfeff90bb292d651dee220c4ac1e2e9be1
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-22 06:59:12 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (63.20%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -12,26 +12,56 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        //bfs
+//         vector<vector<int>>v;
+//                 if(!root)return v;
+
+//         queue<pair<TreeNode*,int>>q;
+//         q.push({root,0});
+//         while(!q.empty()){
+//                         vector<int>p;
+// int size=q.size();
+//         for(int i=0;i<size;i++){
+//             auto node=q.front();
+//             q.pop();
+//             p.push_back(node.first->val);
+//             if(node.first->left){
+//                 q.push({node.first->left,node.second-1});
+//             }
+//             if(node.first->right){
+//                 q.push({node.first->right,node.second+1});
+//             }
+//         }
+//             v.push_back(p);
+//         }
+//         return v;
+        //dfs
         vector<vector<int>>v;
                 if(!root)return v;
 
-        queue<pair<TreeNode*,int>>q;
-        q.push({root,0});
+        queue<TreeNode*>q;
+        q.push(root);
+        q.push(NULL);
+                                vector<int>p;
+
         while(!q.empty()){
-                        vector<int>p;
-int size=q.size();
-        for(int i=0;i<size;i++){
-            auto node=q.front();
+TreeNode* node=q.front();
             q.pop();
-            p.push_back(node.first->val);
-            if(node.first->left){
-                q.push({node.first->left,node.second-1});
+            if(node==NULL){
+                v.push_back(p);
+                p.clear();
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }else{
+                p.push_back(node->val);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
             }
-            if(node.first->right){
-                q.push({node.first->right,node.second+1});
-            }
-        }
-            v.push_back(p);
         }
         return v;
     }
