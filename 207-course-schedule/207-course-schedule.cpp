7ee17d318bf54f1f126fea2deab18dfc13e99635
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-08-27 10:02:39 +0530

    Time: 23 ms (87.94%), Space: 14.9 MB (20.26%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<vector<int>>graph(n);
-        vector<int>indegree(n,0),bfs;
-        for(auto it:pre){
-            graph[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
-        }
-        for(int i=0;i<n;i++){
-            if(!indegree[i]){
-                bfs.push_back(i);
-            }
-        }
+        ///create graph
+        unordered_map<int,vector<int>>graph;
         
-        for(int i=0;i<bfs.size();i++ ){
-            for(auto j:graph[bfs[i]]){
-                if(--indegree[j]==0){
-                    bfs.push_back(j);
+        vector<int>indegree(n);
+        for(auto it:pre){
+            int ai=it[0];
+            int bi=it[1];
+            graph[bi].push_back(ai);
+            indegree[ai]++;
+        }
+        queue<int>q;
+        int count=0;
+for(int i=0;i<n;i++){
+    if(indegree[i]==0){
+        q.push(i);
+    }
+}
+        while(!q.empty()){
+            int index=q.front();
+            q.pop();
+            count++;
+           vector<int>children=graph[index];
+            for(int i:children){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.push(i);
                 }
             }
         }
-        return bfs.size()==n;
+        return count==n;
     }
 };
