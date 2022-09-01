61722d97824fa7858e476160bc2faa9d8cc7cec5
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-02 00:20:21 +0530

    Time: 3 ms (82.35%), Space: 7.4 MB (98.59%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    void recurrperm(vector<int>nums,vector<vector<int>>&ans,int index){
-    if(index==nums.size()){
-        ans.push_back(nums);
-        return;
-    }
-        for(int i=index;i<nums.size();i++){
-            swap(nums[index],nums[i]);
-            recurrperm(nums,ans,index+1);
-            swap(nums[i],nums[index]);
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        recurrperm(nums,ans,0);
+        sort(nums.begin(),nums.end());
+        do{
+            ans.push_back(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
         return ans;
     }
 };
