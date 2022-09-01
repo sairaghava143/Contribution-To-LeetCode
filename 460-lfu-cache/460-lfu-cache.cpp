5a726c0c0198f5bf2eda2c361f5fe3378c4b64ed
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-01 15:46:33 +0530

    Time: 566 ms (92.49%), Space: 184.9 MB (68.35%) - LeetHub

--- 460-lfu-cache/460-lfu-cache.cpp
+++ 460-lfu-cache/460-lfu-cache.cpp
@@ -26,18 +26,18 @@ public:
         {   if(cap!=0)
             {  
             if(size>=cap)
-            {                                                    //removing page
-               auto temp = lst_map[min_frequency].back();        //making copy of popped element
-               lst_map[min_frequency].pop_back();                //popping it out
-               key_map.erase(temp.first);                        //popping from key_map
-               frequency_map.erase(temp.first);                  //popping from frequency_map;
-                                                                 // done removing
+            {                                                    
+               auto temp = lst_map[min_frequency].back();        
+               lst_map[min_frequency].pop_back();                
+               key_map.erase(temp.first);                       
+               frequency_map.erase(temp.first);                 
+                                                                
             }
             else
                 size++;
-            lst_map[1].emplace_front(make_pair(key,value));      // adding node in list
+            lst_map[1].emplace_front(make_pair(key,value));      
             min_frequency=1;
-            key_map[key]= lst_map[1].begin();                    // adding new key iterator pair in key map
+            key_map[key]= lst_map[1].begin();                   
             frequency_map[key]=1;
             
         }
@@ -45,14 +45,14 @@ public:
         else
         {
             auto frequency = frequency_map[key];
-            lst_map[frequency].erase(found->second);             //removed from old list
-            if(lst_map[frequency].size()==0)                     //in case list becomes empty it might change min_frequency
+            lst_map[frequency].erase(found->second);             
+            if(lst_map[frequency].size()==0)                    
             {
                 if(min_frequency ==frequency)
                     min_frequency++;
             }
-            lst_map[++frequency].emplace_front(key,value);       //added in new list
-            key_map[key]=lst_map[frequency].begin();             //updated in keymap
+            lst_map[++frequency].emplace_front(key,value);       
+            key_map[key]=lst_map[frequency].begin();            
             frequency_map[key]=frequency; 
             
         }
