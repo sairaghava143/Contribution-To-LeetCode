7b0325c12e2c3323948c3480f08491cf874ec2f8
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-01 23:29:47 +0530

    Time: 173 ms (17.61%), Space: 52.3 MB (19.23%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>&ans, int target,vector<int>& candidates,vector<int>v,int &n,int index){
+    void helper(vector<vector<int>>&ans, int &target,vector<int>& candidates,vector<int>v,int &n,int index,int sum){
         if(index>=n){
-            if(target==0){
+            if(target==sum){
                 
                             ans.push_back(v);
 
@@ -11,18 +11,19 @@ public:
             return;
         }
 
-        if(candidates[index]<=target){
+        if(sum+candidates[index]<=target ){
         
-
+sum+=candidates[index];
         v.push_back(candidates[index]);
-        helper(ans,target-candidates[index],candidates,v,n,index);
+        helper(ans,target,candidates,v,n,index,sum);
+sum-=candidates[index];
 
                    v.pop_back();
 
 
         }
              
-                helper(ans,target,candidates,v,n,index+1);
+                helper(ans,target,candidates,v,n,index+1,sum);
 
         
 
@@ -37,7 +38,7 @@ public:
         int index=0;
         int n=candidates.size();
         int sum=0;
-        helper(ans,target,candidates,v,n,index);
+        helper(ans,target,candidates,v,n,index,sum);
         return ans;
     }
 };
