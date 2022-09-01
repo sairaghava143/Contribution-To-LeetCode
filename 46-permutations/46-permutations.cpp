92d191d665b93ce9b62928f9812c1907cd043504
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-02 00:39:18 +0530

    Time: 13 ms (9.65%), Space: 8.1 MB (27.41%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -6,7 +6,7 @@ public:
             return;
         }
         
-        for(int i=index;i<n;i++){
+        for(int i=0;i<n;i++){
             if(!temp[i]){
                 temp[i]=1;
                 v.push_back(nums[i]);
