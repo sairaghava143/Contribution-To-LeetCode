685ac3cc199830a86ae229d6750bcdcddff106b3
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-02 18:44:19 +0530

    Time: 5 ms (10.07%), Space: 5.9 MB (78.86%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-    return n==0?"":convertToTitle(--n/26)+char('A'+n%26);
+        if(n==0){
+            return "";
+        }
+                n--;
+
+       string ans= convertToTitle(n/26)+char('A'+n%26);
+        
+        return ans;
     }
 };
