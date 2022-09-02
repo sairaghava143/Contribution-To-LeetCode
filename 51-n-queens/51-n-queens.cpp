eb37b09758255164efff3611ea5efa4cdc893f91
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-02 16:25:18 +0530

    Time: 24 ms (29.30%), Space: 11.9 MB (24.35%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,82 +1,52 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> s,int n){
-        int duprow=row,dupcol=col;
-        while(row>=0 && col>=0){
-            if(s[row][col]=='Q')return false;
-            row--;
-            col--;
+    bool isSafe(int row,int col,vector<string> &v,int &n){
+        int duprow=row;
+        int dupcol=col;
+  
+        while(dupcol>=0){
+            if(v[duprow][dupcol--]=='Q')return false;
         }
-    row=duprow,col=dupcol;
-        while(col>=0){
-            if(s[row][col]=='Q')return false;
-            col--;
+    duprow=row;
+    dupcol=col;
+            
+        while(dupcol>=0 && duprow>=0){
+            if(v[duprow--][dupcol--]=='Q')return false;
         }
-    row=duprow,col=dupcol;
-
-        while(row<n && col>=0){
-            if(s[row][col]=='Q')return false;
-            row++;
-            col--;
+    duprow=row;
+    dupcol=col;
+            
+        while(dupcol>=0 && duprow<n){
+            if(v[duprow++][dupcol--]=='Q')return false;
         }
         return true;
     }
-    void solve(vector<vector<string>>&ans,vector<string>s,int n,int col){
-        if(col==n){
-            ans.push_back(s);
+    void solve(int n,vector<string>v,vector<vector<string>>&ans,int col){
+        if(col>=n){
+                    ans.push_back(v);
             return;
         }
         
-        for(int row=0;row<n;row++){
-            if(isSafe(row,col,s,n)){
-                s[row][col]='Q';
-                solve(ans,s,n,col+1);
-                s[row][col]='.';
-            }
-        }
-    }
-    
-    void solve2(vector<vector<string>>&ans,vector<string>s,int n,int col,vector<int>rowleft,vector<int>lowerbound,vector<int>upperbound){
-        if(col==n){
-            ans.push_back(s);
-            return;
-        }
-        for(int row=0;row<n;row++){
-            if(rowleft[row]==0 && lowerbound[row+col]==0 && upperbound[n-1+col-row]==0){
-                s[row][col]='Q';
-                rowleft[row]=1;
-                lowerbound[row+col]=1;
-                upperbound[n-1+col-row]=1;
-                solve2(ans,s,n,col+1,rowleft,lowerbound,upperbound);
-                s[row][col]='.';
-                rowleft[row]=0;
-                lowerbound[row+col]=0;
-                upperbound[n-1+col-row]=0;
+        for(int i=0;i<n;i++){
+            if(isSafe(i,col,v,n)){
+              v[i][col]  ='Q';
+                solve(n,v,ans,col+1);
+              v[i][col] ='.';
+                
+                
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        ///brute force
-//         vector<vector<string>>ans;
-//         vector<string>s(n);
-//         string st(n,'.');
-//         for(int i=0;i< n;i++){
-//             s[i]=st;
-//         }
-        
-//         solve(ans,s,n,0);
-//         return ans;
-        
-        //optimized approach
         vector<vector<string>>ans;
-        vector<int> rowleft(n,0),lowerbound(2*n-1,0),upperbound(2*n-1,0);
-        vector<string>s(n);
-        string st(n,'.');
-        for(int i=0;i< n;i++){
-            s[i]=st;
+        string s(n,'.');
+        vector<string>v(n);
+        for(int i=0;i<n;i++){
+            v[i]=s;
         }
         
-        solve2(ans,s,n,0,rowleft,lowerbound,upperbound);
+
+        solve(n,v,ans,0);
         return ans;
     }
 };
