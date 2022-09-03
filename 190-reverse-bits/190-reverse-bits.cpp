7d61fd18564bce45fb1a4f7f505f62b569d5a338
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-03 20:22:25 +0530

    Time: 3 ms (66.81%), Space: 6.1 MB (11.23%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -3,7 +3,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
         string s=bitset<32>(n).to_string();
         reverse(s.begin(),s.end());
-        n=bitset<32>(s).to_ulong();
-    return n;
+        return bitset<32>(s).to_ulong();
+        
     }
 };
