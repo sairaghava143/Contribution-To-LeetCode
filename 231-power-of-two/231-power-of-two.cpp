6da9c46cf481f43be55bfa5db36a72ce190bf431
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-05 00:35:53 +0530

    Time: 7 ms (14.98%), Space: 6.1 MB (27.35%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int x=INT_MAX;
-    for(int i=0;i<x;i++){
-        long long s=pow(2,i);
-        if(s>n)return false;
-        if(s==n)return true;
-    
-    }
-        return false;
-   
+        if(!n)return false;
+        return ceil(log2(n))==floor(log2(n));
     }
 };
