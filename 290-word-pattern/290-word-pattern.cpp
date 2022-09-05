9e037ff18f5feaf8c99795c0d579c2c0fe663343
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-05 20:22:21 +0530

    Time: 4 ms (29.91%), Space: 6.4 MB (54.20%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string str) {
-        unordered_map<char, int> p2i;
-    unordered_map<string, int> w2i;
-    istringstream in(str);
-    int i = 0, n = pattern.size();
-    for (string word; in >> word; i++) {
-        if (i == n || p2i[pattern[i]] != w2i[word])
-            return false;
-        p2i[pattern[i]] = w2i[word] = i + 1;
-    }
-    return i == n;
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,int>m1;
+        unordered_map<string,int>m2;
+        istringstream ss(s);
+        string word;
+        int i=0;
+        int n=pattern.size();
+        while(ss>>word){
+            if(i>=n || m1[pattern[i]]!=m2[word])return false;
+            m1[pattern[i]]=m2[word]=i+1;
+            i++;
+        }
+        return i==n;
+            
     }
 };
