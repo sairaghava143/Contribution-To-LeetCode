bb3a27eab78f69825818d581487b223679aa2723
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-05 19:26:31 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (67.77%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -9,16 +9,13 @@ public:
         
         while(s<e){
             mid=s+(e-s)/2;
-            // if(mid<=0)return 1;
             if(isBadVersion(mid)){
                 e=mid;
             }else{
                 s=mid+1;
             }
-            // s++;
-            // e--;
         }
-        if(e<=0)return 1;
+        // if(e<=0)return 1;
         return e;
     }
 };
