acf870a5c29c3bee1b9d11af83281433216a1be8
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-07 20:51:08 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (97.77%) - LeetHub

--- 401-binary-watch/401-binary-watch.cpp
+++ 401-binary-watch/401-binary-watch.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<string> readBinaryWatch(int turnedOn) {
-         vector<string> rs;
-    for (int h = 0; h < 12; h++)
-    for (int m = 0; m < 60; m++)
-        if (bitset<10>(h << 6 | m).count() == turnedOn)
-            rs.emplace_back(to_string(h) + (m < 10 ? ":0" : ":") + to_string(m));
-    return rs;
+        vector<string>v;
+        for(int i=0;i<12;i++){
+            for(int j=0;j<60;j++){
+                if(__builtin_popcount(i)+__builtin_popcount(j)==turnedOn){
+                    v.push_back(to_string(i)+(j<10?":0":":")+to_string(j));
+                }
+            }
+        }
+        return v;
     }
 };
