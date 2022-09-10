ff85464f8e29d484c779935ae74e55c21631cda9
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-10 16:23:31 +0530

    Time: 3 ms (40.74%), Space: 5.9 MB (26.88%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        string s=bitset<32>(x).to_string();
-        string t=bitset<32>(y).to_string();
-        int n1=s.size(),n2=t.size();
-int count=0;
-        for(int i=0;i<n1;i++){
-if(s[i]=='0'&& t[i]=='1')count++;
-            if(s[i]=='1'&& t[i]=='0')count++;
-
+        int count=0;
+        while(x or y){
+            count+=(x&1)!=(y&1);
+            x>>=1,y>>=1;
+            
         }
- 
         return count;
-
     }
 };
