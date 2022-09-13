8e134314510d54c7096b20955bfff3cb7b4e0fce
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-13 13:58:13 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.74%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int findComplement(int num) {
         unsigned mask=~0;
-        while(mask & num)mask<<=1;
-        return ~mask & ~num;
-        
+        while(num & mask)mask<<=1;
+        return ~mask&~num;
     }
 };
