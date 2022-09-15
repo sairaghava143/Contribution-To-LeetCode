a1cd09a738fba7b761e174ab57c034605d2e35e8
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-15 19:20:14 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (33.06%) - LeetHub

--- 492-construct-the-rectangle/492-construct-the-rectangle.cpp
+++ 492-construct-the-rectangle/492-construct-the-rectangle.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     vector<int> constructRectangle(int area) {
-        long long l=INT_MAX,w=INT_MAX;
-        for(int i=1;i<=area;i++){
-            for(int j=1;j<=i;j++){
-        if(i*j>area){
-            break;
+        for(int mid=sqrt(area);mid>=0;mid--){
+            if(!(area%mid)){
+                return {area/mid,mid};
+            }
         }
-                if(i*j==area){
-                  if(l>i && w>j){
-                    l=i,w=j;
-        return {int(l),int(w)};
-
-                }   
-                }
-               
-        }
-        }
-        return {int(l),int(w)};
+        return {};
     }
 };
