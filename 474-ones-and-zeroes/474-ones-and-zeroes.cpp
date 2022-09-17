3b64512ca9c8beb6517906dea57c1c6070dccb62
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-17 12:12:32 +0530

    Time: 458 ms (55.86%), Space: 90 MB (33.68%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-           vector<vector<vector<int>>>dp;
-    int findmax(vector<string>& strs, int m, int n,int index ,int &s){
+    int findmax(vector<string>& strs, int m, int n,int index ,int &s, vector<vector<vector<int>>>&dp){
         if(index>=s)return 0;
         if(dp[m][n][index]!=-1)return dp[m][n][index];
         int countzeros=count(strs[index].begin(),strs[index].end(),'0');
         int countones=strs[index].size()-countzeros;
         
         if(m-countzeros>=0 && n-countones>=0){
-            
-            return dp[m][n][index]= max(1+findmax(strs,m-countzeros,n-countones,index+1,s),findmax(strs,m,n,index+1,s));
+            return dp[m][n][index]= max(1+findmax(strs,m-countzeros,n-countones,index+1,s,dp),findmax(strs,m,n,index+1,s,dp));
         }
         
-                    return dp[m][n][index]=findmax(strs,m,n,index+1,s);
-
+        return dp[m][n][index]=findmax(strs,m,n,index+1,s,dp);
 
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
         int s=strs.size();
-        dp.resize(m+1,vector<vector<int>>(n+1,vector<int>(s+1,-1)));
-        return findmax(strs,m,n,0,s);
+        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(s+1,-1)));
+        return findmax(strs,m,n,0,s,dp);
     }
 };
