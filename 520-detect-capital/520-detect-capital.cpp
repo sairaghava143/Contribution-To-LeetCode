a117971aa8643516d2478a9d37954bad4867891c
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-09-23 00:05:28 +0530

    Time: 6 ms (16.55%), Space: 6.2 MB (19.16%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -3,10 +3,12 @@ public:
     bool detectCapitalUse(string word) {
         int count1=0,count2=0,count3=0;
         bool check=false;
-        for(auto &it:word){
-            if(it>='A' && it<='Z')count1++;
-            if(it>='a' && it<='z')count2++;
-            if((it>='A' && it<='Z' && !check)||(it>='a' && it<='z') ){count3++;check=true;}
+        int n=word.size();
+        for(int i=0;i<n;i++){
+            if(word[i]>='A' && word[i]<='Z')count1++;
+            if(word[i]>='a' && word[i]<='z')count2++;
+            if((word[i]>='A' && word[i]<='Z' && !check)||(word[i]>='a' && word[i]<='z') ){count3++;check=true;}
+            if(!(count1==i+1 || count2==i+1 || count3==i+1))return false;
         }
         if(count1==word.size())return true;
         if(count2==word.size())return true;
