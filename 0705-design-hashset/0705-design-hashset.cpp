d4c8d616c2d1e924a8836786c3c7af8c323a76a7
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-11-07 01:36:02 +0530

    Time: 242 ms (41.17%), Space: 41.5 MB (64.75%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,27 @@
+class MyHashSet {
+public:
+    unordered_set<int>s;
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        s.insert(key);
+    }
+    
+    void remove(int key) {
+        s.erase(key);
+    }
+    
+    bool contains(int key) {
+        return s.find(key)!=s.end();
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
