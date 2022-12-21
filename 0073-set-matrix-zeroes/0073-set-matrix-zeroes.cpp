9b5fe6224d3014c2d78ef38dde87840e8326d84f
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-12-21 16:50:29 +0530

    Time: 47 ms (5.74%), Space: 13.3 MB (56.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,26 +4,46 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        // flags to track whether the current row and column contain a cell with value 0
-        vector<bool> row(m, false);
-        vector<bool> col(n, false);
+        bool row0 = false;
+        bool col0 = false;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+                    if (i == 0) row0 = true;
+                    if (j == 0) col0 = true;
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
         
-        // set cells to 0 if the corresponding row or column flag is true
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
                     matrix[i][j] = 0;
                 }
             }
+        }
+        
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (row0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if (col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+    }
         }
     }
 };
