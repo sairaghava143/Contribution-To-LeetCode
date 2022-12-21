426f74561a8b14fd3a96eccbed4303177b30f741
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-12-21 20:01:55 +0530

    Time: 23 ms (12.75%), Space: 11.1 MB (88.82%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,66 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        //time O(n)  space O(n)
+        // unordered_map<Node*,Node*>m;
+        // Node *ptr=head;
+        // while(ptr){
+        //     m[ptr]=new Node(ptr->val);
+        //     ptr=ptr->next;
+        // }
+        // ptr=head;
+        // while(ptr){
+        //     m[ptr]->next=m[ptr->next];
+        //     m[ptr]->random=m[ptr->random];
+        //     ptr=ptr->next;
+        // }
+        // return m[head];
+        
+        
+          //time O(n)  space O(1)
+        //https://leetcode.com/problems/copy-list-with-random-pointer/discuss/1059181/C%2B%2B-or-Three-Pass-or-O(n)-0ms-Beats-100-or-Explanation-(with-example)
+        Node *node=head;
+        while(node){
+            Node *temp=node->next;
+            node->next=new Node(node->val);
+            node->next->next=temp;
+            node=temp;
+        }
+        node =head;
+        while(node){
+            if(node->random){
+                node->next->random=node->random->next;
+            }
+            node=node->next->next;
+        }
+        
+        Node *ans=new Node(0);
+        Node *temp=ans;
+      
+        node=head;
+        while(node){
+            temp->next=node->next;
+            temp=temp->next;
+            
+            node->next=node->next->next;
+            node=node->next;
+        }
+        return ans->next;
+    }
+};
