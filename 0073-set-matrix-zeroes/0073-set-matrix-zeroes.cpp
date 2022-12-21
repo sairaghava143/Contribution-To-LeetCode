8d208647c04ddc30b219238a06206b347f8850be
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-12-21 16:47:43 +0530

    Time: 20 ms (75.27%), Space: 13.3 MB (56.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
-  void setZeroes(vector<vector<int>>& matrix) {
-    int m = matrix.size();
-    int n = matrix[0].size();
-    bool set_first_row = false;
-    bool set_first_col = false;
-    
-    // Check if first row and first column need to be set to 0
-    for (int i = 0; i < m; i++) {
-        if (matrix[i][0] == 0) {
-            set_first_col = true;
-            break;
-        }
-    }
-    for (int j = 0; j < n; j++) {
-        if (matrix[0][j] == 0) {
-            set_first_row = true;
-            break;
-        }
-    }
-    
-    // Set first element of each row and column to 0 if corresponding element in matrix is 0
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < n; j++) {
-            if (matrix[i][j] == 0) {
-                matrix[i][0] = 0;
-                matrix[0][j] = 0;
-            }
-        }
-    }
-    
-    // Set each element to 0 if either first element of the row or column is 0
-    for (int i = 1; i < m; i++) {
-        for (int j = 1; j < n; j++) {
-            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-    
-    // Set first row and first column to 0 if necessary
-    if (set_first_row) {
-        for (int j = 0; j < n; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    if (set_first_col) {
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        // flags to track whether the current row and column contain a cell with value 0
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+        
         for (int i = 0; i < m; i++) {
-            matrix[i][0] = 0;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        
+        // set cells to 0 if the corresponding row or column flag is true
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
         }
     }
-}
-
 };
