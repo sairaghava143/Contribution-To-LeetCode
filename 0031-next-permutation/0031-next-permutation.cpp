8627710dd7420b5512bc4f1112fc81debf9b1c09
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-12-21 17:46:23 +0530

    Time: 7 ms (62.43%), Space: 12.2 MB (37.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if(nums.size()<=1)return;
-        int i=nums.size()-2;
-        while(i>=0 && nums[i]>=nums[i+1])i--;
-        int j=nums.size()-1;
-        if(i>=0){
-            while(j>=0 && nums[j]<=nums[i])j--;
-            swap(nums[i],nums[j]);
-        }
-        std::next_permutation(nums.begin()+i+1,nums.end());
+        std::next_permutation(nums.begin(),nums.end());
     }
 };
