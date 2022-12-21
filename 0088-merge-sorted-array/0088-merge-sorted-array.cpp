f4c5c10db615fdd1ddaba4a333377268f8e7b897
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-12-21 19:33:56 +0530

    Time: 8 ms (24.62%), Space: 9.2 MB (31.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,23 +1,42 @@
+
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    int i = m - 1, j = n - 1, k = m + n - 1;
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
-    }
-    while (j >= 0) {
-        nums1[k] = nums2[j];
-        j--;
-        k--;
-    }
-}
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    // Create a new vector to store the merged array
+    vector<int> merged(m + n);
 
+    // Initialize variables to keep track of the current index in each array
+    int i = 0, j = 0, k = 0;
+
+    // While both arrays have elements remaining
+    while (i < m && j < n) {
+        // Compare the elements at the current indices and add the smaller one to the merged array
+        if (nums1[i] < nums2[j]) {
+            merged[k] = nums1[i];
+            i++;
+        } else {
+            merged[k] = nums2[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Add any remaining elements from nums1 to the merged array
+    while (i < m) {
+        merged[k] = nums1[i];
+        i++;
+        k++;
+    }
+
+    // Add any remaining elements from nums2 to the merged array
+    while (j < n) {
+        merged[k] = nums2[j];
+        j++;
+        k++;
+    }
+
+    // Copy the merged array back into nums1
+    nums1 = merged;
+}
 
 };
