b32159ace005b8588031e1c98ffe60122af84fd6
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2022-12-26 02:51:27 +0530

    Time: 11 ms (78.18%), Space: 11.5 MB (20.08%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    string largestNumber(vector<int>& a) {
-         std::string largestnumber = "";
-    for (int i = 0; i < a.size() - 1; i++) {
-        for (int j = i + 1; j < a.size(); j++) {
-            std::string temp1 = std::to_string(a[i]) + std::to_string(a[j]);
-            std::string temp2 = std::to_string(a[j]) + std::to_string(a[i]);
-            if (std::stoll(temp2) > std::stoll(temp1)) {
-                std::swap(a[i], a[j]);
-            }
-        }
+bool compare(string a, string b) {
+    // Concatenate the two numbers and compare them
+    return a + b > b + a;
+}
+
+string largestNumber(vector<int>& nums) {
+    // Edge case: if all numbers are 0, return "0"
+    if (all_of(nums.begin(), nums.end(), [](int x){ return x == 0; })) {
+        return "0";
     }
-    for (int i = 0; i < a.size(); i++) {
-        largestnumber += std::to_string(a[i]);
+    
+    // Convert the numbers to strings
+    vector<string> str_nums;
+    for (int num : nums) {
+        str_nums.push_back(to_string(num));
     }
-        if(largestnumber[0]=='0')return "0";
-    return largestnumber;
+    
+    // Sort the numbers using the comparison function
+    sort(str_nums.begin(), str_nums.end(), [](string a,string b){
+         return a + b > b + a;
+    });
+    
+    // Concatenate the sorted numbers to get the final result
+    string result;
+    for (string str : str_nums) {
+        result += str;
     }
+    return result;
+}
 };
