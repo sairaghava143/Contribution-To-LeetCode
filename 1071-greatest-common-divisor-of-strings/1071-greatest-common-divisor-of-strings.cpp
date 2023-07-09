e3a84dfedb1f9193d81aa730f0aa33d183279d4c
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-09 15:36:49 +0530

    Time: 21 ms (6.91%), Space: 31.7 MB (6.76%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-string cant1=str1+str2;
-        string cant2=str2+str1;
-if(cant1!=cant2)return "";
-        int x=gcd(str1.size(),str2.size());
-        
-        return cant1.substr(0,x);
-        
+        string base=(str1.size()<str2.size())?str1:str2;
+     for(int i=base.size();i>=1;i--){
+         string cand=str1.substr(0,i);
+         
+         if(isDivisor(str1,cand) && isDivisor(str2,cand)){
+             return cand;
+         }
+     }
+        return "";
     }
+    
+    bool isDivisor(string str,string cand){
+        if(str.size()%cand.size()!=0)return false;
+        
+        int rep=str.size()/cand.size();
+        string concat;
+        for(int i=0;i<rep;i++){
+            concat+=cand;
+        }
+                return concat==str;
+
+    }
+    
 };
