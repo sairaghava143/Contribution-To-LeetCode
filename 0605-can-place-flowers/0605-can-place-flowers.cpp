03df7b160411ceaccfa67f8e8d15c16df2b507f5
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-09 16:49:48 +0530

    Time: 17 ms (77.42%), Space: 20.2 MB (95.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -9,6 +9,7 @@ public:
         }
         int x=0;
         for(int i=0;i<n;i++){
+            if(n<=x)return true;
             if(i-1>=0 && i+1<n && flowerbed[i-1]==0 && flowerbed[i]==0 &&flowerbed[i+1]==0 ){
                 x++;
                 flowerbed[i]=1;
