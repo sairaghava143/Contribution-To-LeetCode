6d51f5f716e334646a478dbf8e7ea0eca64ad6d6
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-09 16:13:08 +0530

    Time: 25 ms (21.25%), Space: 20.3 MB (66.73%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(n==0)return true;
-        int count=0;
-        int m=flowerbed.size();
-        if(m<=1){
-            if(n<=1 && flowerbed[0]==0)return true;
-            return false;
-        }
-        if(m<=2){
-            if(n<=1){
-                if(flowerbed[0]==1 || flowerbed[1]==1)return false;
-                    return true;
-            }
-            return false;
+    bool canPlaceFlowers(vector<int>& flowerbed, int m) {
         
+    int n=flowerbed.size();
+        if(n<2 && m==1){
+            if(flowerbed[0]==1)return false;
+            return true;
         }
-        for(int i=0;i<m;i++){
-            if(flowerbed[i]==0){
-                if(i==0 && i+1<m && flowerbed[i+1]==0){
-                    flowerbed[i]=1;
-                    count++;
-                }
+        int x=0;
+        for(int i=0;i<n;i++){
+            if(i-1>=0 && i+1<n && flowerbed[i-1]==0 && flowerbed[i]==0 &&flowerbed[i+1]==0 ){
+                x++;
+                flowerbed[i]=1;
                 
-              else  if(i-1>=0 && flowerbed[i-1]==0 && i+1<m && flowerbed[i+1]==0){
-                    flowerbed[i]=1;
-                    count++;
+            }
+            if(i==0){
+                if(flowerbed[i]==0  && i+1<n && flowerbed[i+1]==0 ){
+                    x++;
+                      flowerbed[i]=1;
                 }
-                
-               else if(i==m-1 && i-1>=0 && flowerbed[i-1]==0){
-                    flowerbed[i]=1;
-                    count++;
+            }
+            if(i==n-1){
+                if(flowerbed[i]==0  && i-1>=0 && flowerbed[i-1]==0 ){
+                    x++;
+                      flowerbed[i]=1;
                 }
             }
         }
-        if(n<=count)return true;
-        return false;
+        return m<=x;
     }
 };
