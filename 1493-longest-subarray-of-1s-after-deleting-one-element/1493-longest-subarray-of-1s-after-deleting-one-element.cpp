5aec40cc5ca1dfc13a7c74265175dcf744b8bb94
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-10 16:12:12 +0530

    Time: 60 ms (7.40%), Space: 46.5 MB (5.14%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int maxlen=0,len=0,j=0,ones=0;
+        //bruteforce
+        // int maxlen=0,len=0;
+        // for(int i=0;i<nums.size();i++){
+        //     int zeores=0;
+        //     for(int j=i;j<nums.size();j++){
+        //         if(nums[j]==0){
+        //             zeores++;
+        //         }
+        //         if(zeores>1){
+        //             break;
+        //         }
+        //         len=j-i;
+        //         maxlen=max(len,maxlen);
+        //     }
+        // }
+        // return maxlen;
         
+        //practice
+        
+        int maxlen=0,len=0,zeores=0,j=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                ones++;
+            if(nums[i]==0)
+            {
+                zeores++;
             }
-            if(ones>1){
-                 if(nums[j]==0){
-                ones--;
-            }
+            if(zeores>1){
+                if(nums[j]==0)zeores--;
                 j++;
             }
-           
             len=i-j;
             maxlen=max(len,maxlen);
-            
         }
         return maxlen;
+        
+//         int maxlen=0,len=0,j=0,ones=0;
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0){
+//                 ones++;
+//             }
+//             if(ones>1){
+//                  if(nums[j]==0){
+//                 ones--;
+//             }
+//                 j++;
+//             }
+           
+//             len=i-j;
+//             maxlen=max(len,maxlen);
+            
+//         }
+//         return maxlen;
     }
 };
