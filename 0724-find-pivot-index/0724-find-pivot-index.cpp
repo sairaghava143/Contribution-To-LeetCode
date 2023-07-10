9e91ea76d2b5446ce04153463726d2e72dced88b
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-10 17:23:40 +0530

    Time: 23 ms (84.60%), Space: 31.1 MB (43.10%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         //brute force
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            int s1=0,s2=0;
-            for(int j=i-1;j>=0;j--){
-                s1+=nums[j];
-            }
-            for(int j=i+1;j<n;j++){
-                s2+=nums[j];
-            }
-            if(s1==s2)return i;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     int s1=0,s2=0;
+        //     for(int j=i-1;j>=0;j--){
+        //         s1+=nums[j];
+        //     }
+        //     for(int j=i+1;j<n;j++){
+        //         s2+=nums[j];
+        //     }
+        //     if(s1==s2)return i;
+        // }
+        // return -1;
+        
+        //pivot index
+      //prefix sum approach
+        
+        int totsum=0;
+        for(auto it:nums){
+            totsum+=it;
+        }
+        
+        int leftsum=0,rightsum=0;
+        for(int i=0;i<nums.size();i++){
+            rightsum=totsum-nums[i]-leftsum;
+            if(rightsum==leftsum)return i;
+            leftsum+=nums[i];
         }
         return -1;
     }
