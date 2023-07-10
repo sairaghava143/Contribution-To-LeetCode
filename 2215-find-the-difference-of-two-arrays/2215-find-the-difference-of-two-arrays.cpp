ea996ecf64a70ced454732cee41bcc0fde691e48
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-10 17:35:06 +0530

    Time: 75 ms (33.76%), Space: 36.1 MB (17.38%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -7,13 +7,13 @@ public:
         for(auto it:nums1)m1[it]++;
                 for(auto it:nums2)m2[it]++;
         for(int i=0;i<nums1.size();i++){
-            if(m2.count(nums1[i])==false){
+            if(!m2.count(nums1[i])){
                 ans[0].push_back(nums1[i]);
                 m2[nums1[i]]++;
             }
         }
         for(int i=0;i<nums2.size();i++){
-            if(m1.count(nums2[i])==false){
+            if(!m1.count(nums2[i])){
                 ans[1].push_back(nums2[i]);
                 m1[nums2[i]]++;
             }
