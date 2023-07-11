231ecd1754a7f41cee522a91624585c4f2740e33
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-11 16:28:08 +0530

    Time: 5 ms (13.07%), Space: 6.4 MB (85.19%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string decodestring(int &i,const string &s){
+       string res; 
+
+             while(i<s.size() && s[i]!=']'){
+                         if(!isdigit(s[i])){
+            res+=s[i];
+            i++;
+        }else{
+         int n=0;
+                 while(i<s.size() && isdigit(s[i])){
+                     n=n*10+s[i]-'0';
+                     i++;
+                 }
+                 
+                 i++;
+                 string t=decodestring(i,s);
+                 i++;
+                 
+                 while(n-->0){
+                     res+=t;
+                 }
+           
+        }
+            
+        
+       
+    }        return res;
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    string decodeString(string s) {
+        // using recurrsive approach
+        
+        int i=0;
+        return decodestring(i,s);
+    }
+};
