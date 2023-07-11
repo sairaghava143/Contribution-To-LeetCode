d611c4685d830c665a58c35922b885d697996c11
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-11 17:02:30 +0530

    Time: 3 ms (39.92%), Space: 11.1 MB (9.04%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,36 +1,84 @@
-class Solution {
+class Solution
+{
 public:
-    string decodestring(int &i,const string &s){
+    string decodestring(int &i, const string &s)
+    {
         string res;
-        while(i<s.size() && s[i]!=']'){
-            if(!isdigit(s[i])){
-                res+=s[i];
+        while (i < s.size() && s[i] != ']')
+        {
+            if (!isdigit(s[i]))
+            {
+                res += s[i];
                 i++;
-                    
-            }else{
-                int n=0;
-                while(i<s.size() && isdigit(s[i])){
-                    n=n*10+s[i]-'0';
-                    i++;
-                    
-                }
-                i++;
-                string t=decodestring(i,s);
-                i++;
-                
-                while(n-->0){
-                    res+=t;
-                }
-                
             }
-            
-            
+            else
+            {
+                int n = 0;
+                while (i < s.size() && isdigit(s[i]))
+                {
+                    n = n * 10 + s[i] - '0';
+                    i++;
+                }
+                i++;
+                string t = decodestring(i, s);
+                i++;
+
+                while (n-- > 0)
+                {
+                    res += t;
+                }
+            }
         }
         return res;
     }
-    string decodeString(string s) {
-        //using recurrsive approach
-        int i=0;
-        return decodestring(i,s);
+    string decodeString(string s)
+    {
+        // using recurrsive approach
+        //  int i=0;
+        //  return decodestring(i,s);
+
+        // stack approach
+
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != ']')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                string t;
+                while (st.top() != '[')
+                {
+                    t = st.top()+t;
+                    st.pop();
+                }
+                st.pop();
+
+                string y;
+                while (!st.empty() && isdigit(st.top()))
+                {
+                    y = st.top()+y;
+                    st.pop();
+                }
+                int k = stoi(y);
+                while (k--)
+                {
+                    for (int i = 0; i < t.size(); i++)
+                    {
+                        st.push(t[i]);
+                    }
+                }
+            }
+        }
+        s = "";
+        while (!st.empty())
+        {
+            s = st.top() + s;
+            st.pop();
+        }
+        return s;
     }
 };
