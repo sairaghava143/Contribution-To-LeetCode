43999f8f071e62bcd6a7bec2c46ee270429cc934
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-11 16:32:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (69.74%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     string decodestring(int &i,const string &s){
-       string res; 
-
-             while(i<s.size() && s[i]!=']'){
-                         if(!isdigit(s[i])){
-            res+=s[i];
-            i++;
-        }else{
-         int n=0;
-                 while(i<s.size() && isdigit(s[i])){
-                     n=n*10+s[i]-'0';
-                     i++;
-                 }
-                 
-                 i++;
-                 string t=decodestring(i,s);
-                 i++;
-                 
-                 while(n-->0){
-                     res+=t;
-                 }
-           
-        }
+        string res;
+        while(i<s.size() && s[i]!=']'){
+            if(!isdigit(s[i])){
+                res+=s[i];
+                i++;
+                    
+            }else{
+                int n=0;
+                while(i<s.size() && isdigit(s[i])){
+                    n=n*10+s[i]-'0';
+                    i++;
+                    
+                }
+                i++;
+                string t=decodestring(i,s);
+                i++;
+                
+                while(n-->0){
+                    res+=t;
+                }
+                
+            }
             
-        
-       
-    }        return res;
-}
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
+            
+        }
+        return res;
+    }
     string decodeString(string s) {
-        // using recurrsive approach
-        
+        //using recurrsive approach
         int i=0;
         return decodestring(i,s);
     }
