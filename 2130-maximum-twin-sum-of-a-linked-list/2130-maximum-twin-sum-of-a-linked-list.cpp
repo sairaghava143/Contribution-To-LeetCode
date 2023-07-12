5574f3ea5768116abdd8dcb86e6922277f4593c5
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-12 14:33:10 +0530

    Time: 291 ms (31.26%), Space: 116.5 MB (98.69%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -13,21 +13,54 @@ public:
     int pairSum(ListNode* head) {
         //brute force
         
-        vector<int>v;
-        while(head){
-            v.push_back(head->val);
-            head=head->next;
+//         vector<int>v;
+//         while(head){
+//             v.push_back(head->val);
+//             head=head->next;
             
+//         }
+//         int maxsum=INT_MIN,sum=0;
+//         int n=v.size();
+//         for(int i=0;i<n;i++){
+//             if(n-1-i>=0 and n-1-i<n){
+//                 sum+=v[i];
+//                 sum+=v[n-i-1];
+//             }
+//             maxsum=max(sum,maxsum);
+//             sum=0;
+//         }
+//         return maxsum;
+        
+        //using slow and fast pointer approach
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-        int maxsum=INT_MIN,sum=0;
-        int n=v.size();
-        for(int i=0;i<n;i++){
-            if(n-1-i>=0 and n-1-i<n){
-                sum+=v[i];
-                sum+=v[n-i-1];
-            }
-            maxsum=max(sum,maxsum);
-            sum=0;
+        
+        //reverse second half
+        
+        ListNode* prev=nullptr;
+        ListNode* next;
+        
+        while(slow){
+            next=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=next;
+        }
+        
+        int maxsum=0;
+        
+        //cal sum of twins and getting maxsum
+        
+        while(prev){
+            maxsum=max(maxsum,head->val+prev->val);
+            head=head->next;
+            prev=prev->next;
         }
         return maxsum;
     }
