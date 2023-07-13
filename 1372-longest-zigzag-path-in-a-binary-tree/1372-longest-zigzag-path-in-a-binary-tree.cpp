b7da2b4ff6f8429f2eb02daa8773b3d2553a7509
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-13 13:36:52 +0530

    Time: 188 ms (40.39%), Space: 94.1 MB (86.88%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,3 +1,44 @@
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int maxsteps=0;
+//     int longestZigZag(TreeNode* root) {
+//         if(!root)return 0;
+        
+//         dfs(root,true,0);
+//         dfs(root,false,0);
+        
+// return maxsteps;
+//     }
+//     void dfs(TreeNode* root,bool isleft,int step){
+//         if(!root)return;
+        
+//         maxsteps=max(maxsteps,step);
+        
+//         if(isleft){
+//             dfs(root->left,false,step+1);
+//             dfs(root->right,true,1);
+
+//         }else{
+//               dfs(root->right,true,step+1);
+//             dfs(root->left,false,1);
+//         }
+        
+//     }
+// };
+
+//practice
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -14,25 +55,20 @@ public:
     int maxsteps=0;
     int longestZigZag(TreeNode* root) {
         if(!root)return 0;
-        
-        dfs(root,true,0);
         dfs(root,false,0);
-        
-return maxsteps;
+        dfs(root,true,0);
+        return maxsteps;
     }
     void dfs(TreeNode* root,bool isleft,int step){
-        if(!root)return;
-        
+        if(!root)return ;
         maxsteps=max(maxsteps,step);
-        
         if(isleft){
             dfs(root->left,false,step+1);
             dfs(root->right,true,1);
-
         }else{
-              dfs(root->right,true,step+1);
+             dfs(root->right,true,step+1);
             dfs(root->left,false,1);
         }
-        
+            
     }
 };
