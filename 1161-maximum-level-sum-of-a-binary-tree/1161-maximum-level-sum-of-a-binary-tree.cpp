4a365da38c344bc4ad085b8bbfe645fff2d5cc01
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-13 14:29:13 +0530

    Time: 186 ms (95.29%), Space: 104.8 MB (94.83%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,30 +11,55 @@
  */
 class Solution {
 public:
-    int maxLevelSum(TreeNode* root) {
-        if(!root)return 0;
-        int maximal=0;
-        queue<TreeNode*>q;
-        q.push(root);
-        int count=0;
-        int maxsum=INT_MIN;
-        while(!q.empty()){
-            int n=q.size();
-            count++;
-            int sum=0;
-            for(int i=0;i<n;i++){
-                TreeNode* temp=q.front();
-                q.pop();
-                sum+=temp->val;
-                if(temp->left)q.push(temp->left);
-                if(temp->right)q.push(temp->right);
-            }
-            if(sum>maxsum){
-                maximal=count;
-                maxsum=sum;
-            }
-            
+    void dfs(vector<int>&v,int level,TreeNode* root){
+        if(!root)return;
+        if(v.size()==level){
+            v.push_back(root->val);
+        }else{
+            v[level]+=root->val;
         }
-        return maximal;
+        
+        dfs(v,level+1,root->left);
+        dfs(v,level+1,root->right);
+
+            
+    }
+    int maxLevelSum(TreeNode* root) {
+        //bfs approach n,n
+//         if(!root)return 0;
+//         int maximal=0;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         int count=0;
+//         int maxsum=INT_MIN;
+//         while(!q.empty()){
+//             int n=q.size();
+//             count++;
+//             int sum=0;
+//             for(int i=0;i<n;i++){
+//                 TreeNode* temp=q.front();
+//                 q.pop();
+//                 sum+=temp->val;
+//                 if(temp->left)q.push(temp->left);
+//                 if(temp->right)q.push(temp->right);
+//             }
+//             if(sum>maxsum){
+//                 maximal=count;
+//                 maxsum=sum;
+//             }
+            
+//         }
+//         return maximal;
+        
+        
+        //dfs approach n,h
+        if(!root)return 0;
+        vector<int>v;
+        dfs(v,0,root);
+        int x=*max_element(v.begin(),v.end());
+        for(int i=0;i<v.size();i++){
+            if(v[i]==x)return i+1;
+        }
+        return 0;
     }
 };
