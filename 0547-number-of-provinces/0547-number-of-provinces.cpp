b3a62e2969857de6ba2c01256dc0cbeff3d92898
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-13 18:39:46 +0530

    Time: 36 ms (19.39%), Space: 15.7 MB (17.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    void bfs(vector<bool>&v,int node,vector<vector<int>>&isConnected){
+    void bfs(int node,vector<bool>&v,vector<vector<int>>& isConnected){
+        v[node]=true;
         queue<int>q;
         q.push(node);
-        v[node]=true;
         while(!q.empty()){
-         node=q.front();
+            node=q.front();
             q.pop();
             for(int i=0;i<isConnected.size();i++){
-                if(isConnected[node][i]&&!v[i]){
+                if(!v[i] && isConnected[node][i]){
                     v[i]=1;
                     q.push(i);
-                    
                 }
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        //bfs approach
+        //practice bfs approach
         int n=isConnected.size();
         vector<bool>v(n,false);
         int nc=0;
         for(int i=0;i<n;i++){
             if(!v[i]){
                 nc++;
-                bfs(v,i,isConnected);
+                bfs(i,v,isConnected);
             }
         }
         return nc;
+        
     }
 };
