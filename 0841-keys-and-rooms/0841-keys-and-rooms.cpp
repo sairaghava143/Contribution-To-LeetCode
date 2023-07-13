8290862854b63a35c7a22ec63f2605a17f7cfd0f
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-13 17:14:04 +0530

    Time: 8 ms (85.59%), Space: 10.7 MB (20.88%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -10,12 +10,32 @@ public:
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         //practice dfs approach
-        int n=rooms.size();
-        vector<bool>v(n,false);
-        dfs(v,0,rooms);
-        for(int i=0;i<n;i++){
-            if(!v[i])return false;
+        // int n=rooms.size();
+        // vector<bool>v(n,false);
+        // dfs(v,0,rooms);
+        // for(int i=0;i<n;i++){
+        //     if(!v[i])return false;
+        // }
+        // return true;
+        
+        
+        //dfs using stack
+        
+        stack<int>dfs;
+        dfs.push(0);
+        unordered_set<int>s={0};
+        while(!dfs.empty()){
+            int i=dfs.top();
+            dfs.pop();
+            for(int j:rooms[i]){
+                if(s.count(j)==0){
+                    dfs.push(j);
+                    s.insert(j);
+                                    if(s.size()==rooms.size())return true;
+
+                    }
+            }
         }
-        return true;
+        return s.size()==rooms.size();
     }
 };
