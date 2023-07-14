00edcf8a54752d3d6cbb936ff97689039c7ad48f
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-14 21:40:53 +0530

    Time: 3 ms (95.76%), Space: 13.1 MB (70.86%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        // bfs approach
+        //bfs practice
         int m=grid.size(),n=grid[0].size();
         int fo=0;
         queue<pair<int,int>>q;
@@ -9,11 +9,11 @@ public:
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2)q.push({i,j});
                 if(grid[i][j]==1)fo++;
+
             }
         }
         if(fo==0)return 0;
         if(q.empty())return -1;
-        
         vector<pair<int,int>>dirs={{1,0},{-1,0},{0,1},{0,-1}};
         int time=-1;
         while(!q.empty()){
@@ -21,13 +21,15 @@ public:
             while(z--){
                 auto it=q.front();
                 q.pop();
-                for(auto dir:dirs){
-                    int i=it.first+dir.first,j=it.second+dir.second;
+                for(auto dir:dirs ){
+                    int i=dir.first+it.first;
+                    int j=dir.second+it.second;
                     if(i>=0 && i<m && j>=0 && j<n && grid[i][j]==1){
                         fo--;
                         grid[i][j]=2;
                         q.push({i,j});
                     }
+
                 }
             }
             time++;
