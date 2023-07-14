46afd3b4f4e3014ed39d3e655c7abd629451bc83
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-14 17:32:57 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (11.59%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,89 +1,70 @@
-class Solution
-{
+class Solution {
 public:
-    void dfs( string start, string end, double &val, bool &found, map<string, int> &visited, map<string, double> &mp, map<string, vector<string>> &graph)
-    {
-        visited[start] = 1;
-        if (found)
-        {
+    void dfs(string u,string v,double &val,bool &found,map<string,int>&vis,map<string,double>&mp,map<string,vector<string>>&graph){
+        vis[u]=1;
+        if(found){
             return;
         }
-
-        for ( auto it : graph[start])
-        {
-            if (visited[it] != 1)
-            {
-                val *= mp[start + "->" + it];
-                if (end == it)
-                {
-                    found = true;
-                    return;
+        
+        for(auto it:graph[u]){
+            if(vis[it]!=1){
+                val*=mp[u+"->"+it];
+               if(v==it){
+                   found=true;
+                    return ;
                 }
-                dfs(it, end, val, found, visited, mp, graph);
-
-                if (found)
-                {
-                    return;
-                }
-                else
-                {
-                    val /= mp[start + "->" + it];
-                }
+                
+                
+                dfs(it,v,val,found,vis,mp,graph);
+               if(found){
+                   return;
+               }else{
+                   val/=mp[u+"->"+it];
+               }
             }
         }
-
-     
     }
-    vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &values, vector<vector<string>> &queries)
-    {
-        map<string, double> mp;
-        map < string, vector<string> >graph;
-        vector<double> ans;
-        for (int i = 0; i < equations.size(); i++)
-        {
-            string u = equations[i][0];
-            string v = equations[i][1];
-
-            mp[u + "->" + v] = values[i];
-            mp[v + "->" + u] = 1 / values[i];
-
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        //practice dfs approach
+        map<string,vector<string>>graph;
+        map<string,double>mp;
+        vector<double>ans;
+        
+        for(int i=0;i<equations.size();i++){
+        string u=equations[i][0];
+        string v=equations[i][1];
+            mp[u+"->"+v]=values[i];
+            mp[v+"->"+u]=1/values[i];
             graph[u].push_back(v);
             graph[v].push_back(u);
+
+
+
         }
-        for (int i = 0; i < queries.size(); i++)
-        {
-           
-
-            string start = queries[i][0];
-            string end = queries[i][1];
-
-            if (graph.find(start) == graph.end() || graph.find(end) == graph.end())
-            {
+        
+        for(int i=0;i<queries.size();i++){
+            string u=queries[i][0];
+            string v=queries[i][1];
+            
+            if(graph.find(u)==graph.end()||graph.find(v)==graph.end()){
                 ans.push_back(-1.0);
-            }
-            else
-            {
-                double val = 1;
-                bool found = false;
-                map<string, int> visited;
-                if (start == end)
-                {
-                    found = true;
+            }else{
+                double val=1;
+                bool found=false;
+                map<string,int>vis;
+                
+                if(u==v){
+                    found=true;
+                }else{
+                    dfs(u,v,val,found,vis,mp,graph);
                 }
-                else
-                {
-                    dfs(start, end, val, found, visited, mp, graph);
-                }
-
-                if (found)
-                {
+                if(found){
                     ans.push_back(val);
-                }
-                else
-                {
+                }else{
                     ans.push_back(-1);
                 }
             }
+
         }
         return ans;
     }
