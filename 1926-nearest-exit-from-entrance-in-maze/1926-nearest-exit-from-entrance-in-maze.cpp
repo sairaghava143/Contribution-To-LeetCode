42dd6c8719f4cdb40890184fe48741e2c329b65a
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-14 18:24:56 +0530

    Time: 174 ms (31.80%), Space: 41.5 MB (21.40%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+    int nearestExit(vector<vector<char>>& maze, vector<int>& ent) {
+        //practice bfs approach for shortestpath
         int rows=maze.size(),cols=maze[0].size();
-        int srow=entrance[0];
-        int scol=entrance[1];
+        int srow=ent[0],scol=ent[1];
         
         vector<pair<int,int>>dirs={{1,0},{-1,0},{0,1},{0,-1}};
-        
-        maze[srow][scol]='+';
-        
-        queue<array<int,3>>q;
+        maze[srow][scol]='+'; // mark the starting point as visited
+        queue<vector<int>>q;
         q.push({srow,scol,0});
         while(!q.empty()){
-            auto [currR,currC,currD]=q.front();
+            vector<int>v=q.front();
             q.pop();
+            int currR=v[0],currC=v[1],currD=v[2];
             for(auto dir:dirs){
                 int nrow=currR+dir.first,ncol=currC+dir.second;
                 
-                if(nrow>=0 && nrow<rows && ncol>=0 && ncol<cols  && maze[nrow][ncol]=='.'){
-                    if(nrow==0 || nrow==rows-1 || ncol==0 || ncol==cols-1){
+                if(nrow>=0 && nrow<rows && ncol>=0 && ncol<cols && maze[nrow][ncol]=='.'){ // check if ncol is within bounds
+                    if(nrow==0||nrow==rows-1||ncol==0||ncol==cols-1){
                         return currD+1;
                     }
-                    maze[nrow][ncol]='+';
-                                    q.push({nrow,ncol,currD+1});
-
+                    maze[nrow][ncol]='+'; // mark the cell as visited
+                    q.push({nrow,ncol,currD+1});
                 }
-                
-                
             }
         }
-return -1;
-        
+        return -1;
     }
 };
