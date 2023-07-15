989bbfb2e7e0191adf62e1bc01f36e63243cdb7b
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-15 15:08:58 +0530

    Time: 211 ms (43.21%), Space: 72.8 MB (49.04%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int c) {
+        //heap approach
+        
         int i=0,j=costs.size()-1;
         priority_queue<int,vector<int>,greater<int>>pq1;
         priority_queue<int,vector<int>,greater<int>>pq2;
@@ -8,11 +10,9 @@ long long ans=0;
         while(k--){
             while(pq1.size()<c && i<=j){
                 pq1.push(costs[i++]);
-                
             }
-             while(pq2.size()<c && i<=j){
+            while(pq2.size()<c && i<=j){
                 pq2.push(costs[j--]);
-                
             }
             int c1=pq1.size()>0?pq1.top():INT_MAX;
             int c2=pq2.size()>0?pq2.top():INT_MAX;
