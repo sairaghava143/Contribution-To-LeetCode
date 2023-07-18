09e2a1ef4ef323606146ccea09bd5691ead338cf
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 14:44:17 +0530

    Time: 3 ms (37.49%), Space: 6 MB (66.43%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,9 +4,10 @@ public:
         //normal approach
         if(n<2)return n;
         int a=0,b=1,c=1,d=0;
-        while(n-->2){
+        while(n>2){
             d=a+b+c;
             a=b,b=c,c=d;
+            n--;
         }
         return c;
         
