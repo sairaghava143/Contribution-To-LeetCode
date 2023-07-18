0eaf4d42cbe6c5971ad2d7ed4889f20ac88f1171
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 16:24:14 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.44%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,30 +1,24 @@
 class Solution {
+
 public:
-// vector<long long>dp;
-
-//     int tile(int n){
-        
-//         if(n<=3)return dp[n];
-//         if(dp[n]!=-1)return dp[n];
-//         return dp[n]=(2*tile(n-1)+tile(n-3))%(int)(1e9+7);
-//     }
-//     int numTilings(int n) {
-   
-//         dp.resize(1001,-1);
-//                 dp[0]=0,dp[1]=1,dp[2]=2,dp[3]=5;
-
-//         return tile(n);
-        
-//     }
-    int numTilings(int n) {
-    vector<long long> dp(1001, 0);
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
-        
-    for (int i = 3; i <= n; i++) {
-        dp[i] = (2 * dp[i-1] + dp[i-3]) % (int)(1e9 + 7);
+    vector<int> dp;
+    
+    int tile(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        if (n == 3) {
+            return 5;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        dp[n] = (2 * tile(n-1) % (int)(1e9+7) + tile(n-3) % (int)(1e9+7)) % (int)(1e9+7);
+        return dp[n];
     }
-    return dp[n];
-}
+    
+    int numTilings(int n) {
+        dp.resize(n+1, -1);
+        return tile(n);
+    }
 };
