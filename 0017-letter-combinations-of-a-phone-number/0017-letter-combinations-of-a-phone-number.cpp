86dd38392547c5f7d99bf97a9e0d09628eb07bb0
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 10:59:07 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    const vector<string> pad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> letterCombinations(string digits) {
-        //practice backtracking approach
-        if(digits.empty())return {};
-        vector<string>res;
-        res.push_back("");
-        for(auto digit:digits){
-            vector<string>temp;
-            for(auto cand:pad[digit-'0']){
-                for(auto s:res){
-                    cout<<s+cand<<endl;
-                    temp.push_back(s+cand);
-                }
-            }
-            res=temp;
-        }
+        if (digits.empty())
+            return {};
+        vector<string> res;
+        const vector<string> pad = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string builder;
+        build(builder, 0, res, digits, pad);
         return res;
-        
+    }
+    void build(string builder, int i, vector<string> &res, string &digits, const vector<string> &pad) {
+        if (i == digits.size()) {
+            res.push_back(builder);
+            return;
+        }
+        int j = digits[i] - '0';
+        for (char c : pad[j]) {
+            build(builder + c, i + 1, res, digits, pad);
+        }
     }
 };
