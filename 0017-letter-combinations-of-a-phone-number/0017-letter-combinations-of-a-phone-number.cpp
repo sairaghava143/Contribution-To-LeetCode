f8cc65138af8295e650c0acf404511841aec6c5c
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 10:37:32 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (19.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     const vector<string> pad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> letterCombinations(string digits) {
+        //practice backtracking approach
         if(digits.empty())return {};
         vector<string>res;
         res.push_back("");
@@ -9,11 +10,13 @@ public:
             vector<string>temp;
             for(auto cand:pad[digit-'0']){
                 for(auto s:res){
+                    cout<<s+cand<<endl;
                     temp.push_back(s+cand);
                 }
             }
             res=temp;
         }
         return res;
+        
     }
 };
