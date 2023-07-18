4ac3c46b1395101229ad9482c70c2950462c354d
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 11:23:39 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (87.16%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
-    void combination(vector<vector<int>> &ans,vector<int>&sum ,int k,int n){
+    //9^k ,k
+//     void combination(vector<vector<int>> &ans,vector<int>&sum ,int k,int n){
+//         if(sum.size()==k && n==0){
+//             ans.push_back(sum);
+//             return;
+//         }
+//         //pick and not pick
+        
+//         for(int i=sum.empty()?1:sum.back()+1;i<=9;i++){
+//             if(n-i<0)break;
+//             sum.push_back(i);
+//             combination(ans,sum,k,n-i);
+//             sum.pop_back();
+//         }
+            
+//     }
+//     vector<vector<int>> combinationSum3(int k, int n) {
+//         vector<vector<int>>ans;
+//         vector<int>sum;
+//         combination(ans,sum,k,n);
+//         return ans;
+//     }
+    
+    //2^9,k
+    void combination(vector<vector<int>> &ans,vector<int>&sum ,int k,int n,int start){
         if(sum.size()==k && n==0){
             ans.push_back(sum);
             return;
         }
+        //pick and not pick
         
-        for(int i=sum.empty()?1:sum.back()+1;i<=9;i++){
+        for(int i=start;i<=9;i++){
             if(n-i<0)break;
             sum.push_back(i);
-            combination(ans,sum,k,n-i);
+            combination(ans,sum,k,n-i,i+1);
             sum.pop_back();
         }
             
@@ -17,7 +42,7 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>ans;
         vector<int>sum;
-        combination(ans,sum,k,n);
+        combination(ans,sum,k,n,1);
         return ans;
     }
 };
