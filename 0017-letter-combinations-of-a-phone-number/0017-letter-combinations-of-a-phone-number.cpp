938078bfb6412345e192942339ac885d8c471212
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 10:27:14 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.88%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    const vector<string> pad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())return {};
+        vector<string>res;
+        res.push_back("");
+        for(auto digit:digits){
+            vector<string>temp;
+            for(auto cand:pad[digit-'0']){
+                for(auto s:res){
+                    temp.push_back(s+cand);
+                }
+            }
+            res.swap(temp);
+        }
+        return res;
+    }
+};
