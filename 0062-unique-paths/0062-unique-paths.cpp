2a05310ce6f2471c7b9ac9042cb447197ea55d17
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 18:13:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (20.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
+    //memo practice
     int uniquePaths(int m, int n) {
-        vector<vector<int>> memo(m, vector<int>(n, -1));
-        return uniquePathsHelper(m-1, n-1, memo);
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return path(m-1,n-1,dp);
     }
-    
-    int uniquePathsHelper(int i, int j, vector<vector<int>>& memo) {
-        if (i == 0 || j == 0) {
-            return 1;
-        }
-        if (memo[i][j] != -1) {
-            return memo[i][j];
-        }
-        int paths = uniquePathsHelper(i-1, j, memo) + uniquePathsHelper(i, j-1, memo);
-        memo[i][j] = paths;
-        return paths;
+    int path(int i,int j,vector<vector<int>>&dp){
+        if(i==0 || j==0)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        return dp[i][j]=path(i-1,j,dp)+path(i,j-1,dp);
     }
 };
