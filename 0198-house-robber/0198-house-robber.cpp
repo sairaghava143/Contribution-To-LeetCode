2451b584e47a238cefde2a3ceb0bf5d258dd5379
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 15:27:36 +0530

    Time: 8 ms (11.94%), Space: 9.6 MB (5.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     //logic
-    //maxmoney[i]=nums[i]+max(maxmoney(nums,i-1),maxmoney(nums,i-2));
+    //maxmoney[i]=nums[i]+max(maxmoney(nums,i-2),maxmoney(nums,i-3));
+    // int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>dp(n,-1);
+    //     return max(maxmoney(nums,n-1,dp),maxmoney(nums,n-2,dp));
+    // }
+    // int maxmoney(vector<int>nums,int n,vector<int>&dp){
+    //     if(n<0)return 0;
+    //     if(n==0 || n==1)return nums[n];
+    //     if(dp[n]!=-1)return dp[n];
+    //     return dp[n]=nums[n]+max(maxmoney(nums,n-2,dp),maxmoney(nums,n-3,dp));
+    // }
+    
+    //another approach
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        return max(maxmoney(nums,n-1,dp),maxmoney(nums,n-2,dp));
+        return maxmoney(nums,n-1,dp);
     }
     int maxmoney(vector<int>nums,int n,vector<int>&dp){
         if(n<0)return 0;
-        if(n==0 || n==1)return nums[n];
+        
         if(dp[n]!=-1)return dp[n];
-        return dp[n]=nums[n]+max(maxmoney(nums,n-2,dp),maxmoney(nums,n-3,dp));
+        return dp[n]=max(maxmoney(nums,n-2,dp)+nums[n],maxmoney(nums,n-1,dp));
     }
 };
