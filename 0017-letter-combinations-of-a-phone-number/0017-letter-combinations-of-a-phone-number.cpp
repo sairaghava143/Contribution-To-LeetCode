714165defbf674d18e55f52b9e910aae7756c02b
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-18 11:00:02 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.27%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    const vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     vector<string> letterCombinations(string digits) {
         if (digits.empty())
             return {};
         vector<string> res;
-        const vector<string> pad = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
         string builder;
-        build(builder, 0, res, digits, pad);
+        build(builder, 0, res, digits);
         return res;
     }
-    void build(string builder, int i, vector<string> &res, string &digits, const vector<string> &pad) {
+    void build(string builder, int i, vector<string> &res, string &digits) {
         if (i == digits.size()) {
             res.push_back(builder);
             return;
         }
         int j = digits[i] - '0';
         for (char c : pad[j]) {
-            build(builder + c, i + 1, res, digits, pad);
+            build(builder + c, i + 1, res, digits);
         }
     }
 };
