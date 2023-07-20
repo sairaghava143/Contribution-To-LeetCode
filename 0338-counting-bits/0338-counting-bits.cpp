cf3ee67b635ad9973cbe190f4e0765eaa3ccb9be
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-20 11:13:47 +0530

    Time: 7 ms (60.09%), Space: 7.8 MB (64.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>v(n+1);
-        for(int i=0;i<=n;i++){
+        //normal approach by observing pattern
+//         vector<int>v(n+1);
+//         for(int i=0;i<=n;i++){
+//             v[i]=v[i/2]+(i%2);
+//         }
+        
+//         return v;
+          vector<int>v(n+1);
+        v[0]=0;
+        for(int i=1;i<=n;i++){
             v[i]=v[i/2]+(i%2);
         }
         
