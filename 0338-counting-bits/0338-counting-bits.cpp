8e5cc625c283367ecd16dbdf6c5751bbbb9d7570
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-20 11:15:30 +0530

    Time: 5 ms (76.41%), Space: 7.8 MB (87.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -8,10 +8,20 @@ public:
 //         }
         
 //         return v;
+        // similar pattern
+//           vector<int>v(n+1);
+//         v[0]=0;
+//         for(int i=1;i<=n;i++){
+//             v[i]=v[i/2]+(i%2);
+//         }
+        
+//         return v;
+        
+        //bits using pattern
           vector<int>v(n+1);
         v[0]=0;
         for(int i=1;i<=n;i++){
-            v[i]=v[i/2]+(i%2);
+            v[i]=v[i>>1]+(i&1);
         }
         
         return v;
