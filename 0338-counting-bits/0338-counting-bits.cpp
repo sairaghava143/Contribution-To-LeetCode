c4ae5082041b493b06ad69faa5519af961e56acc
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-20 12:08:50 +0530

    Time: 10 ms (32.71%), Space: 7.9 MB (64.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        //normal approach by observing pattern
-//         vector<int>v(n+1);
-//         for(int i=0;i<=n;i++){
-//             v[i]=v[i/2]+(i%2);
-//         }
-        
-//         return v;
-        // similar pattern
-//           vector<int>v(n+1);
-//         v[0]=0;
-//         for(int i=1;i<=n;i++){
-//             v[i]=v[i/2]+(i%2);
-//         }
-        
-//         return v;
-        
-        //bits using pattern
-          vector<int>v(n+1);
-        v[0]=0;
-        for(int i=1;i<=n;i++){
-            v[i]=v[i>>1]+(i&1);
+        //bruteforce
+        vector<int>v(n+1);
+        for(int i=0;i<=n;i++){
+            v[i]=__builtin_popcount(i);
         }
-        
         return v;
     }
 };
