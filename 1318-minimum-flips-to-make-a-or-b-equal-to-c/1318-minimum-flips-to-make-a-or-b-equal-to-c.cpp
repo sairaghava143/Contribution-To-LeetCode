fda6b051d8b4b7e5cc593e29574deea2db526112
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-20 13:48:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (95.38%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int flip1=(__builtin_popcount((a|b)^c));
-        int flip2=(__builtin_popcount(a&b&((a|b)^c)));
-                   return flip1+flip2;
+        //bruteforce approach
+        int flips=0;
+        while(a>0||b>0||c>0){
+            int bitA=a&1;
+            int bitB=b&1;
+            int bitC=c&1;
+
+            if((bitA|bitB)!=bitC){
+                if(bitC==0){
+                    flips+=(bitA+bitB);
+                }else{
+                flips++;
+            }
+            }
+            a>>=1;
+            b>>=1;
+            c>>=1;
+        }
+        return flips;
+        //optimized approach
+        
+        // int flip1=(__builtin_popcount((a|b)^c));
+        // int flip2=(__builtin_popcount(a&b&((a|b)^c)));
+        //            return flip1+flip2;
 
     }
 };
