51cf608eefa29f3f7111e59f7686ba717fe6c26c
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-07-21 16:39:16 +0530

    Time: 166 ms (47.17%), Space: 75.6 MB (52.88%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -2,7 +2,8 @@ class trienode{
     public:
     map<char,trienode*>child;
     vector<string>suggestions;
-    void addsuggestions(const string &product){
+    
+    void addsuggestions(string &product){
         if(suggestions.size()<3){
             suggestions.push_back(product);
         }else{
@@ -14,41 +15,49 @@ class trienode{
         }
     }
 };
+
 class trie{
     public:
     trienode* root;
     trie(){
         root=new trienode();
     }
-    void insert(const string &product){
+    
+    void insert(string product){
         trienode* node=root;
-        for(char c:product){
-            if(!node->child[c]){
-                node->child[c]=new trienode();
+        for(auto it:product){
+            if(!node->child[it]){
+                node->child[it]=new trienode();
             }
-            node =node->child[c];
+            node=node->child[it];
             node->addsuggestions(product);
         }
     }
+    
     vector<string>getsuggestions(trienode* node){
         return node?node->suggestions:vector<string>();
     }
-    vector<vector<string>>search(const string& searchword){
+    
+    vector<vector<string>>search(string searchword){
         vector<vector<string>>result;
         trienode* node=root;
-        for(char c:searchword){
-            node=node?node->child[c]:nullptr;
+        for(auto it:searchword){
+            node =node?node->child[it]:nullptr;
+    
             result.push_back(getsuggestions(node));
-        }
+       
+    }
         return result;
     }
 };
+
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        //trie practice
         sort(products.begin(),products.end());
         trie Trie;
-        for(const string & product:products){
+        for(auto &product:products){
             Trie.insert(product);
         }
         return Trie.search(searchWord);
