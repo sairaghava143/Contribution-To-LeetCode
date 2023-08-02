e291b8e355c5f34baaca1334b4111ac0f69e0112
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-08-02 14:34:52 +0530

    Time: 31 ms (90.08%), Space: 89.2 MB (93.82%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Make sure nums1 is smaller than or equal to nums2
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        
-        int m = nums1.size();
-        int n = nums2.size();
-        int left = 0;
-        int right = m;
-        
-        while (left <= right) {
-            int partition1 = (left + right) / 2;
-            int partition2 = (m + n + 1) / 2 - partition1;
-            
-            int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
-            int minRight1 = (partition1 == m) ? INT_MAX : nums1[partition1];
-            int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
-            int minRight2 = (partition2 == n) ? INT_MAX : nums2[partition2];
-            
-            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
-                if ((m + n) % 2 == 0) {
-                    return (double) (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
-                } else {
-                    return (double) max(maxLeft1, maxLeft2);
-                }
-            } else if (maxLeft1 > minRight2) {
-                right = partition1 - 1;
-            } else {
-                left = partition1 + 1;
-            }
-        }
-        
-        return -1.0;
+  
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    if (nums1.size() > nums2.size()) {
+        return findMedianSortedArrays(nums2, nums1);
     }
+    
+    int x = nums1.size();
+    int y = nums2.size();
+    int start = 0;
+    int end = x;
+    
+    while (start <= end) {
+        int partitionX = (start + end) / 2;
+        int partitionY = (x + y + 1) / 2 - partitionX;
+        
+        int maxOfLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minOfRightX = (partitionX == x) ? INT_MAX : nums1[partitionX];
+        
+        int maxOfLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minOfRightY = (partitionY == y) ? INT_MAX : nums2[partitionY];
+        
+        if (maxOfLeftX <= minOfRightY && maxOfLeftY <= minOfRightX) {
+            if ((x + y) % 2 == 0) {
+                return (max(maxOfLeftX, maxOfLeftY) + min(minOfRightX, minOfRightY)) / 2.0;
+            } else {
+                return max(maxOfLeftX, maxOfLeftY);
+            }
+        } else if (maxOfLeftX > minOfRightY) {
+            end = partitionX - 1;
+        } else {
+            start = partitionX + 1;
+        }
+    }
+    
+    throw invalid_argument("Input arrays are not sorted");
+}
+
 };
