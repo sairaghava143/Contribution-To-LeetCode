747d96e50d98b663717a305133fcb04270f53a5f
sairaghava143 <99303864+sairaghava143@users.noreply.github.com>
2023-09-10 16:46:35 +0530

    Time: 26 ms (50.33%), Space: 16.4 MB (43.27%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,55 +1,33 @@
 class Solution {
 public:
-    void bfs(int node,vector<bool>&v,vector<vector<int>>& isConnected){
-        v[node]=true;
-        queue<int>q;
-        q.push(node);
-        while(!q.empty()){
-            node=q.front();
-            q.pop();
-            for(int i=0;i<isConnected.size();i++){
-                if(!v[i] && isConnected[node][i]){
-                    v[i]=1;
-                    q.push(i);
-                }
-            }
-        }
-    }
-    
-    void dfs(vector<bool>&v,int node,vector<vector<int>>& isConnected){
-        v[node]=true;
-        for(int i=0;i<isConnected.size();i++){
-            if(!v[i] && isConnected[node][i]){
-                dfs(v,i,isConnected);
-                
+    void dfs(vector<int>adjlist[],int i,vector<int>&v){
+        v[i]=1;
+        for(auto &it:adjlist[i]){
+            if(!v[it]){
+                v[it]=1;
+                dfs(adjlist,it,v);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        //practice bfs approach
-        // int n=isConnected.size();
-        // vector<bool>v(n,false);
-        // int nc=0;
-        // for(int i=0;i<n;i++){
-        //     if(!v[i]){
-        //         nc++;
-        //         bfs(i,v,isConnected);
-        //     }
-        // }
-        // return nc;
-        
-        //dfs appraoch
-        
         int n=isConnected.size();
-        vector<bool>v(n);
-        int nc=0;
+        vector<int>adjlist[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adjlist[i].push_back(j);
+                }
+            }
+        }
+        vector<int>v(n,0);
+        int count=0;
         for(int i=0;i<n;i++){
             if(!v[i]){
-                nc++;
-                dfs(v,i,isConnected);
+                dfs(adjlist,i,v);
+                count++;
                 
             }
         }
-        return nc;
+        return count;
     }
 };
